b416c5b09cdcc2a7a18ded456a75d089e8443a97
sureba73 <sureba@expressbase.com>
2016-12-08 12:56:17 +0530

    Initial commit - samples only

---
+++ Startup.cs
@@ -0,0 +1,95 @@
+﻿using Funq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ServiceStack;
+using ServiceStack.Data;
+using ServiceStack.Host.Handlers;
+using ServiceStack.Logging;
+using ServiceStack.Mvc;
+using ServiceStack.OrmLite;
+
+namespace RazorRockstars.WebHost
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseServiceStack(new AppHost());
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Use(new RazorHandler("/notfound"));
+
+            //Other examples of using built-in ServiceStack Handlers as middleware
+            //app.Use(new StaticFileHandler("wwwroot/img/react-logo.png").Middleware);
+            //app.Use(new RequestInfoHandler().Middleware);
+        }
+    }
+
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("Test Razor", typeof(AppHost).GetAssembly()) { }
+
+        public override void Configure(Container container)
+        {
+            Plugins.Add(new RazorFormat());
+
+            container.Register<IDbConnectionFactory>(
+                new OrmLiteConnectionFactory(":memory:", SqliteDialect.Provider));
+
+            using (var db = container.Resolve<IDbConnectionFactory>().Open())
+            {
+                db.CreateTableIfNotExists<Rockstar>();
+                db.InsertAll(RockstarsService.SeedData);
+            }
+
+            //Also works but it's recommended to handle 404's by registering at end of .NET Core pipeline
+            //this.CustomErrorHttpHandlers[HttpStatusCode.NotFound] = new RazorHandler("/notfound");
+
+            SetConfig(new HostConfig { DebugMode = true });
+        }
+    }
+}
