29319f973e73960fc64ea051e603c7c335e4a121
sureba73 <sureba@expressbase.com>
2016-12-28 18:32:36 +0530

    separated artifacts in service stack

---
+++ Artifacts/ServiceRequests.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.ServiceStack
+{
+    [Route("/ebo")]
+    [Route("/ebo/{Id}")]
+    public class EbObjectRequest : IReturn<EbObjectResponse>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Artifacts/ServiceResponses.cs
@@ -0,0 +1,17 @@
+﻿using ServiceStack.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace ExpressBase.ServiceStack
+{
+    [DataContract]
+    [Csv(CsvBehavior.FirstEnumerable)]
+    public class EbObjectResponse
+    {
+        [DataMember(Order = 1)]
+        public List<EbObjectWrapper> Data { get; set; }
+    }
+}
