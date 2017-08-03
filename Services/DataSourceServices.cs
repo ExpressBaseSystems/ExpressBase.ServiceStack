7aa85c781a57096ed2f39cf6ca9ad4e6726225b6
RobyAbraham <robyabraham@expressbase.com>
2017-08-03 06:20:30 -0700

    sqldecoded ....

--- Services/DataSourceServices.cs
+++ Services/DataSourceServices.cs
@@ -41,7 +41,7 @@ namespace ExpressBase.ServiceStack
             if (dt.Rows.Count > 0)
             {
                 var _ds = EbSerializers.Json_Deserialize<EbDataSource>(dt.Rows[0][0].ToString());
-                this.Log.Info("_ds *****" + _ds.Sql);
+                this.Log.Info("_ds *****" + _ds.SqlDecoded());
                 string _sql = string.Empty;
 
                 if (_ds != null)
@@ -67,7 +67,7 @@ namespace ExpressBase.ServiceStack
                         }
                     }
 
-                    _sql = _ds.Sql.Replace("@and_search", _c);
+                    _sql = _ds.SqlDecoded().Replace("@and_search", _c);
                 }
                 this.Log.Info("search ok");
                 _sql = _sql.Replace("@orderby",
@@ -148,9 +148,9 @@ WHERE
                     var _ds = EbSerializers.Json_Deserialize<EbDataSource>(dt.Rows[0][0].ToString());
                     if (_ds != null)
                     {
-                        Log.Info(">>>>>>>>>>>>>>>>>>>>>>>> dscolumns Sql: " + _ds.Sql);
+                        Log.Info(">>>>>>>>>>>>>>>>>>>>>>>> dscolumns Sql: " + _ds.SqlDecoded());
 
-                        string _sql = _ds.Sql.Replace("@and_search", string.Empty).Replace("@orderby", "1");
+                        string _sql = _ds.SqlDecoded().Replace("@and_search", string.Empty).Replace("@orderby", "1");
                         _isPaged = (_sql.ToLower().Contains("@offset") && _sql.ToLower().Contains("@limit"));
 
                         var parameters = new List<System.Data.Common.DbParameter>();
