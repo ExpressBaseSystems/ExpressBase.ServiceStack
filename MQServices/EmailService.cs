6c65ddf66159c59a44adfb650319be0d5b7044fe
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-10-03 17:20:18 +0530

    Bini Email Service

--- MQServices/EmailService.cs
+++ MQServices/EmailService.cs
@@ -1,48 +1,39 @@
-﻿using ExpressBase.Objects.Objects.MQRelated;
-using MailKit.Net.Smtp;
+﻿using MailKit.Net.Smtp;
 using MimeKit;
 using ServiceStack;
+using ExpressBase.Objects.ServiceStack_Artifacts;
+using System;
+using System.Threading.Tasks;
 
 namespace ExpressBase.ServiceStack
 {
     public class EmailService : Service
     {
-        public object Any(EmailRequest req)
+        public async Task<object> PostAsync(EmailServicesMqRequest request)
         {
 
-            string FromAddress = "ahammedunni@expressbase.com";
-            string FromAdressTitle = req.FromAdressTitle;
-            //To Address  
-            string ToAddress = req.ToAddress;
-            string ToAdressTitle = req.ToAdressTitle;
-            string Subject = req.Subject;
-            string BodyContent = req.BodyContent;
+            var emailMessage = new MimeMessage();
 
-            string SmtpServer = "smtp.zoho.com";
-
-            //Smtp Port Number  
-            int SmtpPortNumber = 465;
-            var mimeMessage = new MimeMessage();
-            mimeMessage.From.Add(new MailboxAddress(FromAdressTitle, FromAddress));
-            mimeMessage.To.Add(new MailboxAddress(ToAdressTitle, ToAddress));
-            mimeMessage.Subject = Subject;
-            mimeMessage.Body = new TextPart("plain")
+            emailMessage.From.Add(new MailboxAddress("EXPRESSbase", "info@expressbase.com"));
+            emailMessage.To.Add(new MailboxAddress("", request.To));
+            emailMessage.Subject = request.Subject;
+            emailMessage.Body = new TextPart("plain") { Text = request.Message };
+            try
             {
-                Text = BodyContent
-            };
-            using (var client = new SmtpClient())
-            {
-
-                client.Connect(SmtpServer, SmtpPortNumber, true);
-                // Note: only needed if the SMTP server requires authentication  
-                // Error 5.5.1 Authentication   
-                client.Authenticate("ahammedunni@expressbase.com", "ZMorp2yaW@ZM");
-                client.Send(mimeMessage);
-                client.Disconnect(true);
+                using (var client = new SmtpClient())
+                {
+                    client.LocalDomain = "www.expressbase.com";
+                    await client.ConnectAsync("smtp.gmail.com", 465, true).ConfigureAwait(false);
+                    await client.AuthenticateAsync(new System.Net.NetworkCredential() { UserName = "expressbasesystems@gmail.com", Password = "ebsystems" }).ConfigureAwait(false);
+                    await client.SendAsync(emailMessage).ConfigureAwait(false);
+                    await client.DisconnectAsync(true).ConfigureAwait(false);
+                }
             }
-
+            catch (Exception e)
+            {
+                return e;
+            }
             return null;
         }
     }
-
 }
