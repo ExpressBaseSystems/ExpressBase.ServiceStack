172c0b465915b07d035ee0236020ea71c28330c4
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-10-12 01:01:22 +0530

    MongoDB

--- MQServices/FileService.cs
+++ MQServices/FileService.cs
@@ -1,6 +1,8 @@
-﻿using ExpressBase.Common.Data;
+﻿using ExpressBase.Common;
+using ExpressBase.Common.Data;
 using ExpressBase.Objects.ServiceStack_Artifacts;
 using MongoDB.Bson;
+using MongoDB.Driver.GridFS;
 using ServiceStack;
 using ServiceStack.Messaging;
 using System;
@@ -28,20 +30,22 @@ namespace ExpressBase.ServiceStack.MQServices
         {
             EbSolutionConnections SolutionConnections = this.Redis.Get<EbSolutionConnections>(string.Format("EbSolutionConnections_{0}", request.TenantAccountId));
 
-            request.IsAsync = false;
-
-            if (request.MetaData == null && request.MetaDataPair != null)
-            {
-                request.MetaData = new BsonDocument();
-
-                request.MetaData.AddRange(request.MetaDataPair as IDictionary);
-            }
-
             if (request.IsAsync)
             {
                 try
                 {
-                    this.MessageProducer3.Publish(new UploadFileMqRequest { FileName = request.FileName, ByteArray = request.ByteArray, BucketName = request.BucketName, TenantAccountId = request.TenantAccountId, MetaData = new BsonDocument(request.MetaData), UserId = request.UserId });
+                    this.MessageProducer3.Publish(new UploadFileMqRequest
+                    {
+                        FileDetails = new FileMeta
+                        {
+                            FileName = request.FileDetails.FileName,
+                            MetaDataDictionary = request.FileDetails.MetaDataDictionary
+                        },
+                        ByteArray = request.ByteArray,
+                        BucketName = request.BucketName,
+                        TenantAccountId = request.TenantAccountId,
+                        UserId = request.UserId
+                    });
                     return "Successfully Uploaded to MQ";
                 }
                 catch (Exception e)
@@ -52,9 +56,28 @@ namespace ExpressBase.ServiceStack.MQServices
 
             else if (!request.IsAsync)
             {
-                string Id = (new TenantDbFactory(request.TenantAccountId, this.Redis)).FilesDB.UploadFile(request.FileName, request.ByteArray, request.BucketName, request.MetaData).ToString();
 
-                this.MessageProducer3.Publish(new ImageResizeMqRequest { ObjectId = Id, FileName = request.FileName, ImageByte = request.ByteArray, MetaData = new BsonDocument(request.MetaData), BucketName = request.BucketName, TenantAccountId = request.TenantAccountId, UserId = request.UserId });
+                string Id = (new TenantDbFactory(request.TenantAccountId, this.Redis)).FilesDB.UploadFile(
+                    request.FileDetails.FileName,
+                    request.FileDetails.MetaDataDictionary,
+                    request.ByteArray,
+                    request.BucketName
+                    ).ToString();
+
+                if (request.FileDetails.ContentType < 100)
+                    this.MessageProducer3.Publish(new ImageResizeMqRequest
+                    {
+                        FileDetails = new FileMeta
+                        {
+                            ObjectId = Id,
+                            FileName = request.FileDetails.FileName,
+                            MetaDataDictionary = request.FileDetails.MetaDataDictionary
+                        },
+                        ImageByte = request.ByteArray,
+                        BucketName = request.BucketName,
+                        TenantAccountId = request.TenantAccountId,
+                        UserId = request.UserId
+                    });
 
                 return Id;
             }
@@ -65,7 +88,7 @@ namespace ExpressBase.ServiceStack.MQServices
         [Authenticate]
         public byte[] Post(DownloadFileRequest request)
         {
-            var FileNameParts = request.FileName.Substring(0, request.FileName.IndexOf('.'))?.Split('_');
+            var FileNameParts = request.FileDetails.FileName.Substring(0, request.FileDetails.FileName.IndexOf('.'))?.Split('_');
 
             if (FileNameParts.Length == 1)
             {
@@ -79,7 +102,7 @@ namespace ExpressBase.ServiceStack.MQServices
 
             else if (FileNameParts.Length == 2 || FileNameParts[0] != null)
             {
-                return (new TenantDbFactory(request.TenantAccountId, this.Redis)).FilesDB.DownloadFile(request.FileName, request.BucketName);
+                return (new TenantDbFactory(request.TenantAccountId, this.Redis)).FilesDB.DownloadFile(request.FileDetails.FileName, request.BucketName);
             }
 
             else
@@ -89,20 +112,37 @@ namespace ExpressBase.ServiceStack.MQServices
         }
 
         [Authenticate]
-        public FindFilesByTagResponse Post(FindFilesByTagRequest request)
+        public List<FileMeta> Post(FindFilesByTagRequest request)
         {
-            var filesList = (new TenantDbFactory(request.TenantAccountId, this.Redis)).FilesDB.FindFilesByTags(request.Filter);
+            List<FileMeta> FileList = new List<FileMeta>();
 
-            FindFilesByTagResponse Response = new FindFilesByTagResponse();
+            List<GridFSFileInfo> filesList = (new TenantDbFactory(request.TenantAccountId, this.Redis)).FilesDB.FindFilesByTags(request.Filter, request.BucketName);
 
-            Response.FileList = new List<FilesInfo>();
-
-            foreach (var element in filesList)
+            foreach (GridFSFileInfo file in filesList)
             {
-                Response.FileList.Add(new FilesInfo { ObjectId = element.Id.ToString(), MetaData = new BsonDocument(element.Metadata) });
-            }
+                string stringType = file.Filename.Split('.')[1];
+                int intType =0;
 
-            return Response;
+                foreach (FileTypes type in Enum.GetValues(typeof(ImageTypes)))
+                {
+                    if (type.ToString() == stringType)
+                    {
+                        intType = (int)type;
+                        break;
+                    }
+                }
+
+                FileList.Add(
+                    new FileMeta
+                    {
+                        ObjectId = file.Id.ToString(),
+                        FileName = file.Filename,
+                        ContentType = intType,
+                        Length = file.Length,
+                        UploadDateTime = file.UploadDateTime
+                    });
+            }
+            return FileList;
         }
 
         [Restrict(InternalOnly = true)]
@@ -116,7 +156,14 @@ namespace ExpressBase.ServiceStack.MQServices
             {
                 try
                 {
-                    string Id = (new TenantDbFactory(request.TenantAccountId, this.Redis)).FilesDB.UploadFile(request.FileName, request.ByteArray, request.BucketName, request.MetaData).ToString();
+                    string Id = (new TenantDbFactory(request.TenantAccountId, this.Redis)).
+                        FilesDB.UploadFile(
+                        request.FileDetails.FileName,
+                        request.FileDetails.MetaDataDictionary,
+                        request.ByteArray,
+                        request.BucketName
+                        ).
+                        ToString();
                 }
                 catch (Exception e)
                 {
@@ -127,21 +174,15 @@ namespace ExpressBase.ServiceStack.MQServices
 
             public string Post(ImageResizeMqRequest request)
             {
-                MemoryStream ms = new MemoryStream(request.ImageByte);
-
-                ms.Position = 0;
-
                 UploadFileMqRequest uploadFileRequest = new UploadFileMqRequest();
-
-                uploadFileRequest.BucketName = request.BucketName;
-
-                uploadFileRequest.MetaData = new BsonDocument();
-
-                uploadFileRequest.MetaData = request.MetaData;
-
+                uploadFileRequest.BucketName = "thumbs";
+                uploadFileRequest.FileDetails = new FileMeta();
+                uploadFileRequest.FileDetails.MetaDataDictionary = request.FileDetails.MetaDataDictionary;
                 uploadFileRequest.TenantAccountId = request.TenantAccountId;
-
                 uploadFileRequest.UserId = request.UserId;
+
+                MemoryStream ms = new MemoryStream(request.ImageByte);
+                ms.Position = 0;
 
                 try
                 {
@@ -151,31 +192,25 @@ namespace ExpressBase.ServiceStack.MQServices
                         {
                             int pixels;
 
-                            if (size == "small")
-                            { pixels = 50; }
-                            else if (size == "medium")
-                            { pixels = 150; }
-                            else if (size == "large")
-                            { pixels = 640; }
+                            if (size == "small"){pixels = 50;}
+                            else if (size == "medium"){pixels = 150;}
+                            else if (size == "large"){pixels = 640;}
                             else break;
 
                             var ImgStream = Resize(img, pixels, pixels);
 
                             request.ImageByte = new byte[ImgStream.Length];
                             ImgStream.Read(request.ImageByte, 0, request.ImageByte.Length);
-
+                            
                             uploadFileRequest.ByteArray = request.ImageByte;
-                            uploadFileRequest.FileName = request.ObjectId + "_" + size + ".jpg";
+                            uploadFileRequest.FileDetails.FileName = request.FileDetails.ObjectId + "_" + size + ".png";
 
                             this.MessageProducer3.Publish(uploadFileRequest);
                         }
                     }
                 }
                 catch (Exception e)
-                {
-
-                }
-
+                {}
                 return null;
             }
 
@@ -201,5 +236,4 @@ namespace ExpressBase.ServiceStack.MQServices
             }
         }
     }
-
 }
