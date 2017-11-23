41a6a178ecdaffcb4d20620a56ead6004c88c49f
Ahammed Unni V P <ahammedunni@expressbase.com>
2017-11-23 19:57:48 +0530

    Auth

--- Auth0/MyFacebookAuthProvider.cs
+++ Auth0/MyFacebookAuthProvider.cs
@@ -66,10 +66,10 @@ namespace ExpressBase.ServiceStack.Auth0
             return base.OnAuthenticated(authService, session, tokens, authInfo);
         }
 
-        public override void OnSaveUserAuth(IServiceBase authService, IAuthSession session)
-        {
-            base.OnSaveUserAuth(authService, session);
-        }
+        //public override void OnSaveUserAuth(IServiceBase authService, IAuthSession session)
+        //{
+        //    base.OnSaveUserAuth(authService, session);
+        //}
 
         public override void Init(IAppSettings appSettings = null)
         {
@@ -81,10 +81,10 @@ namespace ExpressBase.ServiceStack.Auth0
             return base.IsAuthorized(session, tokens, request);
         }
 
-        public override string GetKeyId()
-        {
-            return base.GetKeyId();
-        }
+        //public override string GetKeyId()
+        //{
+        //    return base.GetKeyId();
+        //}
        
     }
 }
