afe78e1e4aa229af26c3a4bfff008d85fc2c43c0
Bini Varghese <binivarghese@expressbase.com>
2017-11-28 11:53:08 +0530

    minor

--- MQServices/EmailService.cs
+++ MQServices/EmailService.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using ServiceStack.Messaging;
 using ExpressBase.Objects.EmailRelated;
 using ExpressBase.Common;
+using ExpressBase.Objects;
+using ExpressBase.Common.Data;
 
 namespace ExpressBase.ServiceStack
 {
@@ -17,8 +19,13 @@ namespace ExpressBase.ServiceStack
         public class EmailServiceInternal : EbBaseService
         {
             public EmailServiceInternal(IMessageProducer _mqp, IMessageQueueClient _mqc) : base(_mqp, _mqc) { }
+
+            
+
+
             public string Post(EmailServicesMqRequest request)
             {
+                var _InfraDb = base.ResolveService<ITenantDbFactory>() as TenantDbFactory;
                 var myService = base.ResolveService<EbObjectService>();
                 var res =(EbObjectParticularVersionResponse)myService.Get(new EbObjectParticularVersionRequest() { RefId = request.refid });
                 EbEmailTemplate ebEmailTemplate = new EbEmailTemplate();
@@ -26,9 +33,25 @@ namespace ExpressBase.ServiceStack
                 {
                      ebEmailTemplate = EbSerializers.Json_Deserialize(element.Json);
                 }
+
+
                 var myDs = base.ResolveService<EbObjectService>();
                 var myDsres = (EbObjectParticularVersionResponse)myDs.Get(new EbObjectParticularVersionRequest() { RefId = ebEmailTemplate.DataSourceRefId });
-               // get sql from ebdatasource and render the sql 
+                // get sql from ebdatasource and render the sql 
+                EbDataSource ebDataSource = new EbDataSource();
+                foreach (var element in myDsres.Data)
+                {
+                    ebDataSource = EbSerializers.Json_Deserialize(element.Json);
+                }
+                var ds = _InfraDb.ObjectsDB.DoQueries(ebDataSource.Sql);
+
+                foreach(var table in ds.Tables)
+                {
+                    foreach(var col in table.Columns)
+                    {
+
+                    }
+                }
 
                 var emailMessage = new MimeMessage();
                 emailMessage.From.Add(new MailboxAddress("EXPRESSbase", "info@expressbase.com"));
