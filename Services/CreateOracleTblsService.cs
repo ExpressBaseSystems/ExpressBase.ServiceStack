3fd417097a01ccc929ff399607e6a4e65ccf28bc
ChinchuCVijayan <chinchucvijayan@expressbase.com>
2018-01-18 10:28:05 +0530

    orclservice

---
+++ Services/CreateOracleTblsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.ServiceStack.Services
+{
+    public class CreateOracleTblsService
+    {
+    }
+}
