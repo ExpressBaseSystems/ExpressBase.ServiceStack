7f2f902056837d09180a47249fc78f8f05878a98
ChinchuCVijayan <chinchucvijayan@expressbase.com>
2018-01-22 18:44:01 +0530

    for transaction and log in oracle db

--- Services/Test.cs
+++ Services/Test.cs
@@ -1,6 +1,7 @@
 ﻿using ExpressBase.Common.Data;
 using ExpressBase.Common.Data.OracleDB;
 using ExpressBase.Objects.ServiceStack_Artifacts;
+using ServiceStack.Logging;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
@@ -14,8 +15,11 @@ namespace ExpressBase.ServiceStack.Services
         public Test(ITenantDbFactory _dbf) : base(_dbf) { }
         public TestResponse Any(TestRequest request)
         {
+            ILog log = LogManager.GetLogger(GetType());
             var con = TenantDbFactory.DataDB.GetNewConnection();
+            log.Info("Connection");
             con.Open();
+            log.Info(".............."+con+"Connection Opened");
             string sql = "INSERT INTO testtb (name) VALUES ('BINI')";
 
             //var cmd = c.GetNewCommand(con, sql);
