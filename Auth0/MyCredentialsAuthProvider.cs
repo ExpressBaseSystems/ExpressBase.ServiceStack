5543f839fc6dc1fe704a0556037e26a60f623907
Febin Carlos <33482855+febincarlos@users.noreply.github.com>
2018-02-07 18:26:31 +0530

    Modified- credentialsauthprovider

--- Auth0/MyCredentialsAuthProvider.cs
+++ Auth0/MyCredentialsAuthProvider.cs
@@ -1,5 +1,4 @@
-﻿using ExpressBase.Common.Constants;
-using ExpressBase.Common.Data;
+﻿using ExpressBase.Common.Data;
 using ExpressBase.Objects.ServiceStack_Artifacts;
 using ExpressBase.Security;
 using ServiceStack;
@@ -7,71 +6,61 @@ using ServiceStack.Auth;
 using ServiceStack.Configuration;
 using ServiceStack.Logging;
 using ServiceStack.Redis;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ExpressBase.ServiceStack.Auth0
 {
-    public class MyCredentialsAuthProvider : CredentialsAuthProvider
+	public class MyCredentialsAuthProvider : CredentialsAuthProvider
     {
         private RedisClient Redis { get; set; }
 
         public MyCredentialsAuthProvider(IAppSettings settings) : base(settings) { }
 
+		private ILog Logger
+		{
+			get { return LogManager.GetLogger(GetType()); }
+		}
+
         public override bool TryAuthenticate(IServiceBase authService, string UserName, string password)
         {
-            ILog log = LogManager.GetLogger(GetType());
+			Logger.Info("In TryAuthenticate method1");
 
-            log.Info("In TryAuthenticate method1");
-            var EbConnectionFactory = authService.ResolveService<IEbConnectionFactory>() as EbConnectionFactory;
+			var request = authService.Request.Dto as Authenticate;
+			var cid = request.Meta.ContainsKey("cid") ? request.Meta["cid"] : string.Empty;
+			var socialId = request.Meta.ContainsKey("socialId") ? request.Meta["socialId"] : string.Empty;
+			var whichContext = request.Meta["wc"].ToLower().Trim();
 
-            log.Info("In TryAuthenticate method2");
+			var EbConnectionFactory = authService.TryResolve<IEbConnectionFactory>() as EbConnectionFactory;
 
-            User _authUser = null;
+			Logger.Info("In TryAuthenticate method2");
+			//string[] app_types = { "Mobile", "Web", "Bot" };
+			//if (request.Meta["context"] == "tc" || request.Meta["context"] == "dc")
+			//	app_types 
 
-            var request = authService.Request.Dto as Authenticate;
+			//if (request.Meta.ContainsKey("signup_tok"))
+			//         {
+			//             cid = CoreConstants.EXPRESSBASE;
+			//             _authUser = User.GetInfraVerifiedUser(EbConnectionFactory.DataDB, UserName, request.Meta["signup_tok"]);
+			//         }
 
-            var cid = request.Meta.ContainsKey("cid") ? request.Meta["cid"] : string.Empty;
-            //var socialId = request.Meta.ContainsKey("socialId") ? request.Meta["socialId"] : string.Empty;
-			//var sso = request.Meta.ContainsKey("sso") ? request.Meta["sso"] : string.Empty;
-
-			if (request.Meta.ContainsKey("signup_tok"))
+			User _authUser = null;
+			if (!string.IsNullOrEmpty(socialId))
             {
-                cid = CoreConstants.EXPRESSBASE;
-                _authUser = User.GetInfraVerifiedUser(EbConnectionFactory.DataDB, UserName, request.Meta["signup_tok"]);
+				_authUser = User.GetDetailsSocial(EbConnectionFactory.DataDB, socialId, whichContext);
+				Logger.Info("TryAuthenticate -> socialId");
             }
-            else if(request.Meta.ContainsKey("socialId"))
-            {
-				//if (cid == CoreConstants.EXPRESSBASE)
-				//{
-				//    log.Info("for tenant login");
-				//    _authUser = (string.IsNullOrEmpty(socialId)) ? User.GetInfraUser(TenantDbFactory.DataDB, UserName, password) : User.GetInfraUserViaSocial(TenantDbFactory.DataDB, UserName, socialId);
-				//    log.Info("#Eb reached 1");
-				//}
-				//else
-				//{
-				//log.Info("for user login");
-				//    _authUser = (string.IsNullOrEmpty(socialId)) ? User.GetDetails(TenantDbFactory.DataDB, UserName, password) : User.GetInfraUserViaSocial(TenantDbFactory.DataDB, socialId);
-				_authUser = User.GetDetailsSocial(EbConnectionFactory.DataDB, request.Meta["socialId"]);
-				log.Info("#Eb reached 2");
-                //}
-            }
-			else if (request.Meta.ContainsKey("sso"))
+			else if (request.Meta.ContainsKey("sso") && whichContext.Equals("dc"))
 			{
-				_authUser = User.GetDetailsSSO(EbConnectionFactory.DataDB, UserName);
-				log.Info("#Eb reached 2");
+				_authUser = User.GetDetailsSSO(EbConnectionFactory.DataDB, UserName, whichContext);
+				Logger.Info("TryAuthenticate -> sso");
 			}
-			else
+			else 
 			{
-				_authUser = User.GetDetailsNormal(EbConnectionFactory.DataDB, UserName, password);
-				log.Info("#Eb reached 2");
+				_authUser = User.GetDetailsNormal(EbConnectionFactory.DataDB, UserName, password, whichContext);
+				Logger.Info("TryAuthenticate -> Normal");
 			}
 
             if (_authUser != null)
             {
-
                 CustomUserSession session = authService.GetSession(false) as CustomUserSession;
                 var redisClient = authService.TryResolve<IRedisClientsManager>().GetClient();
                 session.CId = cid;
@@ -80,41 +69,35 @@ namespace ExpressBase.ServiceStack.Auth0
                 session.Email = _authUser.Email;
                 session.IsAuthenticated = true;
                 session.User = _authUser;
-                session.WhichConsole = request.Meta["wc"];
-                _authUser.wc = request.Meta["wc"];
-                session.UserAuthId = string.Format("{0}-{1}-{2}", cid, _authUser.Email, request.Meta["wc"]);
+                session.WhichConsole = whichContext;
+                _authUser.wc = whichContext;
+                session.UserAuthId = string.Format("{0}-{1}-{2}", cid, _authUser.Email, whichContext);
 
                 var authRepo = HostContext.AppHost.GetAuthRepository(authService.Request);
-                var existingUser = (authRepo as EbRedisAuthRepository).GetUserAuth(string.Format("{0}-{1}-{2}", cid, _authUser.Email, request.Meta["wc"]));
-                //if (existingUser != null)
+                var existingUser = (authRepo as EbRedisAuthRepository).GetUserAuth(session.UserAuthId);
                 (authRepo as EbRedisAuthRepository).UpdateUserAuth(existingUser, _authUser);
-                //redisClient.Set<IUserAuth>(string.Format("{0}-{1}", cid, _authUser.Email), _authUser);
             }
 
             return (_authUser != null);
         }
 
         public override object Authenticate(IServiceBase authService, IAuthSession session, Authenticate request)
-            {
-            ILog log = LogManager.GetLogger(GetType());
-
-            log.Info("In Authenticate method1");
-            AuthenticateResponse authResponse = base.Authenticate(authService, session, request) as AuthenticateResponse;
-            log.Info("In Authenticate method2");
+        {
+			Logger.Info("Authenticate -> Start");
+			AuthenticateResponse authResponse = base.Authenticate(authService, session, request) as AuthenticateResponse;
             var _customUserSession = authService.GetSession() as CustomUserSession;
             _customUserSession.WhichConsole = request.Meta.ContainsKey("wc") ? request.Meta["wc"] : string.Empty;
 
             if (!string.IsNullOrEmpty(authResponse.SessionId) && _customUserSession != null)
             {
-                log.Info("In Authenticate method3");
+				Logger.Info("In Authenticate method3");
                 return new MyAuthenticateResponse
                 {
                     UserId = _customUserSession.UserAuthId,
                     UserName = _customUserSession.UserName,
                     User = _customUserSession.User
-            };
+				};
             }
-            
 
             return authResponse;
         }
