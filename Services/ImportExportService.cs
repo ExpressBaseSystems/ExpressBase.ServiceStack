8f01cdf91bc77c412fd6bc8e99d3676e907e9fb5
DonajoseUllattil <donajose@expressbase.com>
2018-07-06 10:33:53 +0530

    Import Export service

---
+++ Services/ImportExportService.cs
@@ -0,0 +1,22 @@
+﻿using ExpressBase.Common.Data;
+using ExpressBase.Objects.ServiceStack_Artifacts;
+using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.ServiceStack.Services
+{
+    public class ImportExportService : EbBaseService
+    {
+        public ImportExportService(IEbConnectionFactory _dbf) : base(_dbf) { }
+
+        
+        //public RelationTreeResponse Get(RelationTreeRequest request)
+        //{
+        //    return new RelationTreeResponse { };
+        //}
+    }
+    
+}
