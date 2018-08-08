ed1ae7ff49ca3464e884a22f33341b5d3c405193
DonajoseUllattil <donajose@expressbase.com>
2018-08-08 09:51:44 +0530

    Added fields in SaveToAppStoreRequest

--- Services/ImportExportService.cs
+++ Services/ImportExportService.cs
@@ -33,13 +33,16 @@ namespace ExpressBase.ServiceStack.Services
                 AppStore _app = new AppStore
                 {
                     Id = Convert.ToInt32(_row[0]),
-                    AppName = _row[1].ToString(),
+                    Name = _row[1].ToString(),
                     Status = Convert.ToInt32(_row[2]),
                     Cost = Convert.ToInt32(_row[4]),
                     CreatedBy = Convert.ToInt32(_row[5]),
                     CreatedAt = Convert.ToDateTime(_row[6]),
                     Json = _row[7].ToString(),
-                    Currency = _row[8].ToString()
+                    Currency = _row[8].ToString(),
+                    AppType= Convert.ToInt32(_row[10]),
+                    Description = _row[11].ToString(),
+                    Icon = _row[12].ToString()
                 };
                 _storeCollection.Add(_app);
             }
@@ -51,16 +54,19 @@ namespace ExpressBase.ServiceStack.Services
             using (DbConnection con = this.InfraConnectionFactory.ObjectsDB.GetNewConnection())
             {
                 con.Open();
-                string sql = @"INSERT INTO eb_appstore (app_name, status, user_solution_id, cost, created_by, created_at, json, currency)
-                                                VALUES (:app_name, :status, :user_solution_id, :cost, :created_by, Now(), :json, :currency)";
+                string sql = @"INSERT INTO eb_appstore (app_name, status, user_solution_id, cost, created_by, created_at, json, currency, app_type, description, icon)
+                                                VALUES (:app_name, :status, :user_solution_id, :cost, :created_by, Now(), :json, :currency, :app_type, :description, :icon)";
                 DbCommand cmd = InfraConnectionFactory.ObjectsDB.GetNewCommand(con, sql);
-                cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":app_name", EbDbTypes.String, request.Store.AppName));
+                cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":app_name", EbDbTypes.String, request.Store.Name));
                 cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":status", EbDbTypes.Int32, request.Store.Status));
                 cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":user_solution_id", EbDbTypes.String, request.TenantAccountId));
                 cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":cost", EbDbTypes.Int32, request.Store.Cost));
                 cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":created_by", EbDbTypes.Int32, request.UserId));
                 cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":json", EbDbTypes.Json, request.Store.Json));
                 cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":currency", EbDbTypes.String, request.Store.Currency));
+                cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":app_type", EbDbTypes.Int32, request.Store.AppType));
+                cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":description", EbDbTypes.String, request.Store.Description));
+                cmd.Parameters.Add(InfraConnectionFactory.ObjectsDB.GetNewParameter(":icon", EbDbTypes.String, request.Store.Icon));
                 object x = cmd.ExecuteScalar();
                 return new SaveToAppStoreResponse { };
             }
