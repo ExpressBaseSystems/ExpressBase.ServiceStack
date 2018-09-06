5f0f784b685ac31452dd1e3275831278d5204a45
Ahammed Unni <ahammedunni@expressbase.com>
2018-09-06 18:27:25 +0530

    cleanup

--- Services/EbFileDownloadService.cs
+++ Services/EbFileDownloadService.cs
@@ -26,89 +26,7 @@ namespace ExpressBase.ServiceStack.Services
         public List<KeyValuePair<int, string>> Files { get; set; }
 
         public EbFileDownloadService(IEbConnectionFactory _dbf, IEbStaticFileClient _sfc, IMessageProducer _mqp, IMessageQueueClient _mqc)
-        : base(_dbf, _sfc, _mqp, _mqc)
-        {
-        }
-
-        //public void ListFilesDirectory(string DirecStructure)
-        //{
-        //    FtpWebRequest request = (FtpWebRequest)WebRequest.Create(FileDownloadConstants.HostName + DirecStructure + "DICOM/");
-        //    request.Timeout = -1;
-        //    request.Method = WebRequestMethods.Ftp.ListDirectory;
-        //    System.Net.ServicePointManager.Expect100Continue = false;
-        //    request.Credentials = new NetworkCredential(UserName, Password);
-
-        //    try
-        //    {
-        //        FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-        //        Stream responseStream = response.GetResponseStream();
-        //        StreamReader reader = new StreamReader(responseStream);
-        //        string[] files = reader.ReadToEnd().Split('\n');
-        //        if (files.Length > 0)
-        //        {
-        //            for (int i = 0; i < files.Length; i++)
-        //            {
-        //                if (!files[i].Trim().IsNullOrEmpty())
-        //                {
-        //                    Console.WriteLine("File Name : " + files[i]);
-        //                    Files.Add(files[i]);
-        //                }
-
-        //            }
-        //        }
-        //        Console.WriteLine($"Directory List Complete, status {response.StatusDescription}");
-        //        reader.Close();
-        //        response.Close();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex);
-        //    }
-        //}
-
-        //private List<string> ListDirectory(string DirecStructure)
-        //{
-        //    List<string> DirectoryListing = new List<string>();
-        //    FtpWebRequest request = (FtpWebRequest)WebRequest.Create(FileDownloadConstants.HostName + DirecStructure);
-        //    request.Timeout = -1;
-        //    request.Method = WebRequestMethods.Ftp.ListDirectory;
-        //    System.Net.ServicePointManager.Expect100Continue = false;
-        //    request.Credentials = new NetworkCredential(UserName, Password);
-        //    try
-        //    {
-        //        FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-        //        Stream responseStream = response.GetResponseStream();
-        //        StreamReader reader = new StreamReader(responseStream);
-        //        string[] Directories = reader.ReadToEnd().Split('\n');
-        //        for (int i = 0; i < Directories.Length; i++)
-        //        {
-        //            //if (!Directories[i].Equals(string.Empty) || !Directories[i].Equals('\r') || !Directories[i].Equals('\n'))
-        //            //{
-        //            //    DirectoryListing.Add(Directories[i]);
-        //            //    Console.WriteLine("Directory Listing: " + Directories[i]);
-        //            //}
-        //            //if (temp.Equals('\r'))
-        //            //    DirectoryListing.Add(temp.Substring(0, temp.Length - 1));
-        //            //else
-        //            //DirectoryListing.Add(temp);
-        //            if (!Directories[i].Equals(string.Empty))
-        //            {
-        //                if (Directories[i][Directories[i].Length - 1].Equals('\r'))
-        //                    DirectoryListing.Add(Directories[i].Substring(0, Directories[i].Length - 1));
-        //                else
-        //                    DirectoryListing.Add(Directories[i]);
-        //            }
-        //        }
-        //        Console.WriteLine($"Directory List Complete, status {response.StatusDescription}");
-        //        reader.Close();
-        //        response.Close();
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex);
-        //    }
-        //    return DirectoryListing;
-        //}
+        : base(_dbf, _sfc, _mqp, _mqc) { }
 
         private void GetFileNamesFromDb()
         {
@@ -141,7 +59,7 @@ namespace ExpressBase.ServiceStack.Services
         {
             int res = 0;
             string MapQuery = @"INSERT into customer_files(customer_id, eb_files_ref_id) values(customer_id=@cust_id, eb_files_ref_id=@ref_id) returning id";
-            DbParameter[] MapParams=
+            DbParameter[] MapParams =
             {
                         this.InfraConnectionFactory.DataDB.GetNewParameter("cust_id", EbDbTypes.Int32, CustomerId),
                         this.InfraConnectionFactory.DataDB.GetNewParameter("ref_id", EbDbTypes.Int32, FileRefId)
