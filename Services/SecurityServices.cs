0790f94bd337500cc432a4d3ecbec52917087ccf
Febin Carlos <33482855+febincarlos@users.noreply.github.com>
2018-09-24 19:13:20 +0530

    ebuser dprefid get

--- Services/SecurityServices.cs
+++ Services/SecurityServices.cs
@@ -141,7 +141,7 @@ namespace ExpressBase.ServiceStack.Services
 			string sql = this.EbConnectionFactory.DataDB.EB_MANAGEUSER_FIRST_QUERY;
 			if (request.Id > 1)
 			{
-				sql += @"SELECT fullname,nickname,email,alternateemail,dob,sex,phnoprimary,phnosecondary,landline,phextension,fbid,fbname,statusid,hide,preferencesjson
+				sql += @"SELECT fullname,nickname,email,alternateemail,dob,sex,phnoprimary,phnosecondary,landline,phextension,fbid,fbname,statusid,hide,preferencesjson,dprefid
 						FROM eb_users WHERE id = :id;
 						SELECT role_id FROM eb_role2user WHERE user_id = :id AND eb_del = 'F';
 						SELECT groupid FROM eb_user2usergroup WHERE userid = :id AND eb_del = 'F';";
@@ -205,6 +205,7 @@ namespace ExpressBase.ServiceStack.Services
 					resp.UserData.Add("statusid", dr[12].ToString());
 					resp.UserData.Add("hide", dr[13].ToString());
 					resp.UserData.Add("preference", dr[14].ToString());
+					resp.UserData.Add("dprefid", dr[15].ToString());
 				}
 
 				resp.UserRoles = new List<int>();
