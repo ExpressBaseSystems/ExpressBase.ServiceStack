672e708610deec2b72f480519933df7a6eb23c7f
Amal K Dev <amaldev@expressbase.com>
2018-11-22 15:27:50 +0530

    form data service

--- Services/ApiServices.cs
+++ Services/ApiServices.cs
@@ -2,6 +2,8 @@
 using ExpressBase.Common.Data;
 using ExpressBase.Common.Structures;
 using ExpressBase.Objects.ServiceStack_Artifacts;
+using Newtonsoft.Json;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -11,13 +13,13 @@ using System.Threading.Tasks;
 
 namespace ExpressBase.ServiceStack.Services
 {
-    public class ApiServices: EbBaseService
+    public class ApiServices : EbBaseService
     {
         public ApiServices(IEbConnectionFactory _dbf) : base(_dbf) { }
 
         public FormDataJsonResponse Post(FormDataJsonRequest request)
         {
-
+            WebformData data = JsonConvert.DeserializeObject<WebformData>(request.JsonData);
 
             return new FormDataJsonResponse { };
         }
