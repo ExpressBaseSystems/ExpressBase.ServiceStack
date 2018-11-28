b9bee2d1b582fbf492b3293da0374e353bdab36d
DonajoseUllattil <donajose@expressbase.com>
2018-11-28 11:33:08 +0530

    Scheduler in Reportbuilder

--- Services/SchedulerServices.cs
+++ Services/SchedulerServices.cs
@@ -1,20 +1,80 @@
-﻿using ExpressBase.Objects.ServiceStack_Artifacts;
+﻿using ExpressBase.Common;
+using ExpressBase.Common.Data;
+using ExpressBase.Common.Structures;
+using ExpressBase.Objects.ServiceStack_Artifacts;
 using ServiceStack.Messaging;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ExpressBase.ServiceStack.Services
 {
-    public class SchedulerServices :EbBaseService
+    public class SchedulerServices : EbBaseService
     {
-        public SchedulerServices(IMessageProducer _mqp, IMessageQueueClient _mqc) : base( _mqp, _mqc) { }
+        public SchedulerServices(IEbConnectionFactory _dbf, IMessageProducer _mqp, IMessageQueueClient _mqc):base(_dbf, _mqp, _mqc) { }
 
         public SchedulerMQResponse Post(SchedulerMQRequest request)
         {
             MessageProducer3.Publish(new SchedulerRequest { Task = request.Task });
             return null;
+        }
+
+        public GetAllUsersResponse Get(GetAllUsersRequest request)
+        {
+            GetAllUsersResponse res = new GetAllUsersResponse();
+            using (var con = this.EbConnectionFactory.ObjectsDB.GetNewConnection())
+            {
+                con.Open();
+                string sql = @"SELECT id, firstname FROM eb_users WHERE statusid = 0 ;
+                             SELECT id, name FROM eb_usergroup WHERE eb_del ='F'";
+                var dt = this.EbConnectionFactory.ObjectsDB.DoQueries(sql);
+
+                Dictionary<int, string> Users = new Dictionary<int, string>();
+                Dictionary<int, string> UserGroups = new Dictionary<int, string>();
+                foreach (EbDataRow dr in dt.Tables[0].Rows)
+                {
+                    Users[Convert.ToInt32(dr[0])] = dr[1].ToString();
+                }
+                foreach (EbDataRow dr in dt.Tables[1].Rows)
+                {
+                    UserGroups[Convert.ToInt32(dr[0])] = dr[1].ToString();
+                }
+                res.Users = Users;
+                res.UserGroups = UserGroups;
+            }
+            return res ;
+        }
+        public GetUserEmailsResponse Get(GetUserEmailsRequest request)
+        {
+            GetUserEmailsResponse res = new GetUserEmailsResponse();
+            using (var con = this.EbConnectionFactory.ObjectsDB.GetNewConnection())
+            {
+                con.Open();
+                string sql = @"SELECT id, email FROM eb_users WHERE id = ANY
+                             (string_to_array(:userids,',')::int[]);
+                           SELECT distinct id, email FROM eb_users WHERE id = ANY(SELECT userid FROM eb_user2usergroup WHERE 
+                                groupid = ANY(string_to_array(:groupids,',')::int[])) ;
+";
+                DbParameter[] parameters = { EbConnectionFactory.ObjectsDB.GetNewParameter("userids", EbDbTypes.String, request.UserIds),
+               EbConnectionFactory.ObjectsDB.GetNewParameter("groupids", EbDbTypes.String,  request.UserGroupIds) };
+                
+                var dt = this.EbConnectionFactory.ObjectsDB.DoQueries(sql, parameters);
+                Dictionary<int, string> Users = new Dictionary<int, string>();
+                Dictionary<int, string> UserGroups = new Dictionary<int, string>();
+                foreach (EbDataRow dr in dt.Tables[0].Rows)
+                {
+                    Users[Convert.ToInt32(dr[0])] = dr[1].ToString();
+                }
+                foreach (EbDataRow dr in dt.Tables[1].Rows)
+                {
+                    UserGroups[Convert.ToInt32(dr[0])] = dr[1].ToString();
+                }
+                res.UserEmails = Users;
+                res.UserGroupEmails = UserGroups;
+            }
+                return res;
         }
     }
 }
