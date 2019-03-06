1dfb1a38d53119188b2217b502f1ff00dd8b70c5
Dona Jose <donajose@expressbase.com>
2019-03-06 11:10:32 +0530

    Sql profiler modifications

--- Services/ProfilerServices.cs
+++ Services/ProfilerServices.cs
@@ -8,32 +8,37 @@ using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using ExpressBase.Common.SqlProfiler;
+using Newtonsoft.Json;
 
 namespace ExpressBase.ServiceStack.Services
 {
     public class ProfilerServices : EbBaseService
     {
         public ProfilerServices(IEbConnectionFactory _dbf) : base(_dbf) { }
-        public GetExecLogsResponse Get(GetExecLogsRequest request)
+
+        public ProfilerQueryResponse Get(ProfilerQueryColumnRequest request)
         {
             List<EbExecutionLogs> _logs = new List<EbExecutionLogs>();
             List<DbParameter> parameters = new List<DbParameter>();
-            string query = @"SELECT id, rows, exec_time, created_by, created_at, refid FROM eb_executionlogs WHERE refid = :refid";
+            string query = @"SELECT id, rows, exec_time, created_by, created_at FROM eb_executionlogs WHERE refid = :refid";
             parameters.Add(EbConnectionFactory.ObjectsDB.GetNewParameter("refid", EbDbTypes.String, request.RefId));
-            EbDataTable dt = EbConnectionFactory.ObjectsDB.DoQuery(query, parameters.ToArray());
-            foreach (var item in dt.Rows)
-            {
-                EbExecutionLogs eb = new EbExecutionLogs
-                {
-                    Id = Convert.ToInt32(item[0]),
-                    Rows = item[1].ToString(),
-                    Exec_time = Convert.ToDecimal(item[2]),
-                    Created_by = Convert.ToInt32(item[3]),
-                    Created_at = Convert.ToDateTime(item[4])
-                };
-                _logs.Add(eb);
-            }
-            return new GetExecLogsResponse { Logs = _logs };
+            var _dt = EbConnectionFactory.ObjectsDB.DoQuery(query, parameters.ToArray());
+            return new ProfilerQueryResponse { ColumnCollection = _dt.Columns, data = _dt.Rows };
+        }
+
+        public ProfilerQueryResponse Get(ProfilerQueryDataRequest request)
+        {
+            List<DbParameter> parameters = new List<DbParameter>();
+            string query = @"SELECT COUNT(id) FROM eb_executionlogs WHERE refid = :refid;
+                            SELECT EL.id, EL.rows, EL.exec_time, EU.fullname, EL.created_at FROM eb_executionlogs EL, eb_users EU
+                                            WHERE
+                                            refid = :refid AND EL.created_by = EU.id
+                                            LIMIT :limit OFFSET :offset;";
+            parameters.Add(EbConnectionFactory.ObjectsDB.GetNewParameter("refid", EbDbTypes.String, request.RefId));
+            parameters.Add(EbConnectionFactory.ObjectsDB.GetNewParameter("limit", EbDbTypes.Int32, request.Length));
+            parameters.Add(EbConnectionFactory.ObjectsDB.GetNewParameter("offset", EbDbTypes.Int32, request.Start));
+            var _ds = EbConnectionFactory.ObjectsDB.DoQueries(query, parameters.ToArray());
+            return new ProfilerQueryResponse { Draw = request.Draw, data = _ds.Tables[1].Rows, RecordsTotal = Convert.ToInt32(_ds.Tables[0].Rows[0][0]), RecordsFiltered = Convert.ToInt32(_ds.Tables[0].Rows[0][0]) };
         }
 
         public GetProfilersResponse Get(GetProfilersRequest request)
@@ -74,6 +79,30 @@ namespace ExpressBase.ServiceStack.Services
 
             return new GetProfilersResponse { Profiler = profiler };
         }
-    }
 
+        public GetLogdetailsResponse Get(GetLogdetailsRequest request)
+        {
+            string sql = "SELECT * FROM eb_executionlogs WHERE id = :id";
+            DbParameter[] p = { EbConnectionFactory.ObjectsDB.GetNewParameter("id", EbDbTypes.Int32, request.Index) };
+            EbDataTable _logdetails = EbConnectionFactory.ObjectsDB.DoQuery(sql, p);
+            EbExecutionLogs ebObj = new EbExecutionLogs { Rows = _logdetails.Rows[0][1].ToString(), Exec_time = Convert.ToInt32(_logdetails.Rows[0][2]), Username = _logdetails.Rows[0][3].ToString(), Created_at = Convert.ToDateTime(_logdetails.Rows[0][4]), Params = JsonConvert.DeserializeObject<List<JsonParams>>(_logdetails.Rows[0][6].ToString()) };
+            return new GetLogdetailsResponse { logdetails = ebObj };
+        }
+
+        public GetChartDetailsResponse Get(GetChartDetailsRequest request)
+        {
+            string sql = "SELECT rows, exec_time FROM eb_executionlogs WHERE refid = :refid AND EXTRACT (month FROM created_at) = EXTRACT(month FROM current_date)";
+            DbParameter[] p = { EbConnectionFactory.ObjectsDB.GetNewParameter("refid", EbDbTypes.String, request.Refid) };
+            EbDataTable _chartdetails = EbConnectionFactory.ObjectsDB.DoQuery(sql, p);
+            return new GetChartDetailsResponse { data = _chartdetails.Rows };
+        }
+
+        public GetChart2DetailsResponse Get(GetChart2DetailsRequest request)
+        {
+            string sql = "SELECT created_at FROM eb_executionlogs WHERE refid = :refid AND created_at::TIMESTAMP::DATE =current_date";
+            DbParameter[] p = { EbConnectionFactory.ObjectsDB.GetNewParameter("refid", EbDbTypes.String, request.Refid) };
+            EbDataTable _chartdetails = EbConnectionFactory.ObjectsDB.DoQuery(sql, p);
+            return new GetChart2DetailsResponse { data = _chartdetails.Rows };
+        }        
+    }
 }
