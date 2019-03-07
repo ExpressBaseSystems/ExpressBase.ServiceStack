a6dd7ac76a1bb50e17f95def1b526b3821df9493
RobyAbraham <robyabraham@expressbase.com>
2019-03-07 15:39:52 +0530

    new services for datasourcedataset

--- Services/DataSourceServices.cs
+++ Services/DataSourceServices.cs
@@ -256,6 +256,210 @@ namespace ExpressBase.ServiceStack
         }
 
         [CompressResponse]
+        public DataSourceDataSetColumnsResponse Get(DataSourceDataSetColumnsRequest request)
+        {
+            string _dsRedisKey = string.Format("{0}_columns", request.RefId);
+            EbDataSet _dataset = null;
+
+            DataSourceDataSetColumnsResponse resp = new DataSourceDataSetColumnsResponse();
+            resp.ColumnsCollection = new List<ColumnColletion>();
+
+            EbDataReader _ds = null;
+            var myService = base.ResolveService<EbObjectService>();
+            var result = (EbObjectParticularVersionResponse)myService.Get(new EbObjectParticularVersionRequest() { RefId = request.RefId });
+            _ds = EbSerializers.Json_Deserialize(result.Data[0].Json);
+            Redis.Set<EbDataReader>(request.RefId, _ds);
+
+            if (_ds != null)
+            {
+                string _sql = string.Empty;
+                _sql = _ds.Sql.Replace("@and_search", string.Empty).Replace("@orderby", "1");
+                bool _isPaged = true;
+
+                var parameters = DataHelper.GetParams(this.EbConnectionFactory, _isPaged, request.Params, 0, 0);
+
+                try
+                {
+                    _dataset = this.EbConnectionFactory.ObjectsDB.DoQueries(_sql, parameters.ToArray<System.Data.Common.DbParameter>());
+
+                    foreach (var dt in _dataset.Tables)
+                        resp.ColumnsCollection.Add(dt.Columns);
+
+                    this.Redis.Set<DataSourceDataSetColumnsResponse>(_dsRedisKey, resp);
+                }
+                catch (Exception e)
+                {
+                    Log.Info(">>>>>>>>>>>>>>>>>>>>>>>> dscolumns e.Message: " + e.Message);
+                    this.Redis.Remove(_dsRedisKey);
+                }
+            }
+
+            return resp;
+        }
+
+        [CompressResponse]
+        public DataSourceDataSetDataResponse Any(DataSourceDataSetDataRequest request)
+        {
+            this.Log.Info("data request");
+
+            DataSourceDataSetDataResponse dsresponse =  new DataSourceDataSetDataResponse();
+
+            var _ds = this.Redis.Get<EbDataReader>(request.RefId);
+
+            if (_ds == null)
+            {
+                var myService = base.ResolveService<EbObjectService>();
+                var result = (EbObjectParticularVersionResponse)myService.Get(new EbObjectParticularVersionRequest() { RefId = request.RefId });
+                _ds = EbSerializers.Json_Deserialize(result.Data[0].Json);
+                Redis.Set<EbDataReader>(request.RefId, _ds);
+            }
+            if (_ds.FilterDialogRefId != string.Empty)
+            {
+                var _dsf = this.Redis.Get<EbFilterDialog>(_ds.FilterDialogRefId);
+                if (_dsf == null)
+                {
+                    var myService = base.ResolveService<EbObjectService>();
+                    var result = (EbObjectParticularVersionResponse)myService.Get(new EbObjectParticularVersionRequest() { RefId = _ds.FilterDialogRefId });
+                    _dsf = EbSerializers.Json_Deserialize(result.Data[0].Json);
+                    Redis.Set<EbFilterDialog>(_ds.FilterDialogRefId, _dsf);
+                }
+                if (request.Params == null)
+                    request.Params = _dsf.GetDefaultParams();
+            }
+            string sql = string.Empty;
+            string countsql = string.Empty;
+            var sqlArray = _ds.Sql.Split(";");
+            sql = sqlArray[request.QueryIndex];
+            if (request.QueryIndex == 0)
+            {
+                string _c = string.Empty;
+                string firstsql = sqlArray[0];
+                if (request.TFilters != null && request.TFilters.Count > 0)
+                {
+                    foreach (TFilters _dic in request.TFilters)
+                    {
+                        var op = _dic.Operator; var col = _dic.Column; var val = _dic.Value; var type = _dic.Type;
+                        var array = _dic.Value.Split("|");
+                        if (array.Length == 0)
+                        {
+                            if (op == "x*")
+                                _c += string.Format("AND LOWER({0}) LIKE LOWER('{1}%') ", col, val);
+                            else if (op == "*x")
+                                _c += string.Format("AND LOWER({0}) LIKE LOWER('%{1}') ", col, val);
+                            else if (op == "*x*")
+                                _c += string.Format("AND LOWER({0}) LIKE LOWER('%{1}%') ", col, val);
+                            else if (op == "=")
+                                _c += string.Format("AND LOWER({0}) = LOWER('{1}') ", col, val);
+                            else
+                                _c += string.Format("AND {0} {1} '{2}' ", col, op, val);
+                        }
+                        else
+                        {
+                            string _cond = string.Empty;
+                            for (int i = 0; i < array.Length; i++)
+                            {
+                                if (array[i].Trim() != "")
+                                {
+                                    if (type == "string")
+                                    {
+                                        if (op == "x*")
+                                            _cond += string.Format(" LOWER({0}) LIKE LOWER('{1}%') OR", col, array[i].Trim());
+                                        else if (op == "*x")
+                                            _cond += string.Format(" LOWER({0}) LIKE LOWER('%{1}') OR", col, array[i].Trim());
+                                        else if (op == "*x*")
+                                            _cond += string.Format(" LOWER({0}) LIKE LOWER('%{1}%') OR", col, array[i].Trim());
+                                        else if (op == "=")
+                                            _cond += string.Format(" LOWER({0}) = LOWER('{1}') OR", col, array[i].Trim());
+                                    }
+                                    else
+                                    {
+                                        if (this.EbConnectionFactory.ObjectsDB.Vendor == DatabaseVendors.ORACLE)
+                                        {
+                                            if (type == "date")
+                                                _cond += string.Format(" {0} {1} date '{2}' OR", col, op, array[i].Trim());
+                                            else
+                                                _cond += string.Format(" {0} {1} '{2}' OR", col, op, array[i].Trim());
+                                        }
+                                        else
+                                            _cond += string.Format(" {0} {1} '{2}' OR", col, op, array[i].Trim());
+                                    }
+                                }
+                            }
+                            int place = _cond.LastIndexOf("OR");
+                            _cond = _cond.Substring(0, place);
+                            _c += "AND (" + _cond + ")";
+                        }
+                    }
+                }
+
+                if (!firstsql.ToLower().Contains(":and_search"))
+                {
+                    firstsql = "SELECT * FROM (" + firstsql + "\n ) data WHERE 1=1 :and_search order by :orderby";
+                }
+                firstsql = firstsql.ReplaceAll(";", string.Empty);
+                firstsql = firstsql.Replace(":and_search", _c) + ";";
+
+                var matches = Regex.Matches(firstsql, @"\;\s*SELECT\s*COUNT\(\*\)\s*FROM");
+                if (matches.Count == 0)
+                {
+                    countsql = firstsql.ReplaceAll(";", string.Empty);
+                    countsql = "SELECT COUNT(*) FROM (" + countsql + ") data1;";
+                }
+
+                firstsql = firstsql.ReplaceAll(";", string.Empty);
+                if (this.EbConnectionFactory.ObjectsDB.Vendor == DatabaseVendors.ORACLE)
+                {
+                    firstsql = "SELECT * FROM ( SELECT a.*,ROWNUM rnum FROM (" + firstsql + ")a WHERE ROWNUM <= :limit+:offset) WHERE rnum > :offset;";
+                    //sql1 += "ALTER TABLE T1 DROP COLUMN rnum;SELECT * FROM T1;";
+                }
+                else
+                {
+                    if (!firstsql.ToLower().Contains(":limit"))
+                        firstsql = firstsql + " LIMIT :limit OFFSET :offset;";
+                }
+                sql = countsql + firstsql;
+            }
+            
+            string __order = string.Empty;
+            if (request.OrderBy != null)
+            {
+                __order = string.Format("{0} {1}", request.OrderBy.Column, (request.OrderBy.Direction == 2) ? "DESC" : "ASC");
+            }
+            sql = sql.Replace(":orderby", (string.IsNullOrEmpty(__order)) ? "2" : __order);
+            bool _isPaged = true;
+            var parameters = DataHelper.GetParams(this.EbConnectionFactory, _isPaged, request.Params, request.Length, request.Start);
+            var _dataset = this.EbConnectionFactory.ObjectsDB.DoQueries(sql, parameters.ToArray<System.Data.Common.DbParameter>());
+            if (GetLogEnabled(request.RefId))
+            {
+                TimeSpan T = _dataset.EndTime - _dataset.StartTime;
+                InsertExecutionLog(_dataset.RowNumbers, T, _dataset.StartTime, request.UserId, request.Params, request.RefId);
+            }
+            int _recordsTotal = 0, _recordsFiltered = 0;
+            if (_isPaged)
+            {
+                if (request.QueryIndex == 0)
+                {
+                    Int32.TryParse(_dataset.Tables[0].Rows[0][0].ToString(), out _recordsTotal);
+                    Int32.TryParse(_dataset.Tables[0].Rows[0][0].ToString(), out _recordsFiltered);
+                }
+                else
+                {
+                    _recordsTotal = _dataset.Tables[0].Rows.Count;
+                    _recordsFiltered = _dataset.Tables[0].Rows.Count;
+                }
+            }
+            _recordsTotal = (_recordsTotal > 0) ? _recordsTotal : _dataset.Tables[1].Rows.Count;
+            _recordsFiltered = (_recordsFiltered > 0) ? _recordsFiltered : _dataset.Tables[1].Rows.Count;
+
+
+            dsresponse.Data = (request.QueryIndex == 0) ? _dataset.Tables[1].Rows : _dataset.Tables[0].Rows;
+            dsresponse.Draw = request.Draw;
+            dsresponse.RecordsTotal = _recordsTotal;
+            dsresponse.RecordsFiltered = _recordsFiltered;
+            return dsresponse;
+        }
+
+        [CompressResponse]
         public DataSourceDataSetResponse Any(DataSourceDataSetRequest request)
         {
             this.Log.Info("data request");
