5c35fae8f16522d64d77eef88037fc59bd7a6bfd
Dona Jose <donajose@expressbase.com>
2019-03-12 09:59:35 +0530

    Profiler related

--- Services/ProfilerServices.cs
+++ Services/ProfilerServices.cs
@@ -53,29 +53,26 @@ namespace ExpressBase.ServiceStack.Services
                              SELECT id, exec_time FROM eb_executionlogs WHERE exec_time=(SELECT MAX(exec_time) FROM eb_executionlogs WHERE refid= :refid and created_at::date = current_date);
                              SELECT id, exec_time FROM eb_executionlogs WHERE exec_time=(SELECT MIN(exec_time) FROM eb_executionlogs WHERE refid= :refid and created_at::date = current_date);
                              SELECT COUNT(*) FROM eb_executionlogs WHERE refid = :refid;
-                             SELECT COUNT(*) FROM eb_executionlogs WHERE created_at::date = current_date;
+                             SELECT COUNT(*) FROM eb_executionlogs WHERE created_at::date = current_date AND refid = :refid;
                              SELECT COUNT(*) FROM eb_executionlogs WHERE extract (month FROM created_at) = extract(month FROM current_date) and refid = :refid;";
             parameters.Add(EbConnectionFactory.ObjectsDB.GetNewParameter("refid", EbDbTypes.String, request.RefId));
             EbDataSet dt = EbConnectionFactory.ObjectsDB.DoQueries(query, parameters.ToArray());
             if (dt.Tables.Count > 0)
-                if (dt.Tables[0].Rows.Count > 0)
-                {
-                    profiler.Max_id = (dt.Tables[0].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[0].Rows[0][0]) : 0;
-                    profiler.Max_exectime = (dt.Tables[0].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[0].Rows[0][1]) : Convert.ToDecimal(0);
-                    profiler.Min_id = (dt.Tables[1].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[1].Rows[0][0]) : 0;
-                    profiler.Min_exectime = (dt.Tables[1].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[1].Rows[0][1]) : Convert.ToDecimal(0);
-                    profiler.Cur_Mon_Max_id = (dt.Tables[2].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[2].Rows[0][0]) : 0;
-                    profiler.Cur_Mon_Max_exectime = (dt.Tables[2].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[2].Rows[0][1]) : Convert.ToDecimal(0);
-                    profiler.Cur_Mon_Min_id = (dt.Tables[3].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[3].Rows[0][0]) : 0;
-                    profiler.Cur_Mon_Min_exectime = (dt.Tables[3].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[3].Rows[0][1]) : Convert.ToDecimal(0);
-                    profiler.Cur_Max_id = (dt.Tables[4].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[4].Rows[0][0]) : 0;
-                    profiler.Cur_Max_exectime = (dt.Tables[4].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[4].Rows[0][1]) : Convert.ToDecimal(0);
-                    profiler.Cur_Min_id = (dt.Tables[5].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[5].Rows[0][0]) : 0;
-                    profiler.Cur_Min_exectime = (dt.Tables[5].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[5].Rows[0][1]) : Convert.ToDecimal(0);
-                    profiler.Total_count = (dt.Tables[6].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[6].Rows[0][0]) : 0;
-                    profiler.Current_count = (dt.Tables[7].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[7].Rows[0][0]) : 0;
-                    profiler.Month_count = (dt.Tables[8].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[8].Rows[0][0]) : 0;
-                }
+                profiler.Max_id = (dt.Tables[0].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[0].Rows[0][0]) : 0;
+            profiler.Max_exectime = (dt.Tables[0].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[0].Rows[0][1]) : Convert.ToDecimal(0);
+            profiler.Min_id = (dt.Tables[1].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[1].Rows[0][0]) : 0;
+            profiler.Min_exectime = (dt.Tables[1].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[1].Rows[0][1]) : Convert.ToDecimal(0);
+            profiler.Cur_Mon_Max_id = (dt.Tables[2].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[2].Rows[0][0]) : 0;
+            profiler.Cur_Mon_Max_exectime = (dt.Tables[2].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[2].Rows[0][1]) : Convert.ToDecimal(0);
+            profiler.Cur_Mon_Min_id = (dt.Tables[3].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[3].Rows[0][0]) : 0;
+            profiler.Cur_Mon_Min_exectime = (dt.Tables[3].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[3].Rows[0][1]) : Convert.ToDecimal(0);
+            profiler.Cur_Max_id = (dt.Tables[4].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[4].Rows[0][0]) : 0;
+            profiler.Cur_Max_exectime = (dt.Tables[4].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[4].Rows[0][1]) : Convert.ToDecimal(0);
+            profiler.Cur_Min_id = (dt.Tables[5].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[5].Rows[0][0]) : 0;
+            profiler.Cur_Min_exectime = (dt.Tables[5].Rows.Count != 0) ? Convert.ToDecimal(dt.Tables[5].Rows[0][1]) : Convert.ToDecimal(0);
+            profiler.Total_count = (dt.Tables[6].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[6].Rows[0][0]) : 0;
+            profiler.Current_count = (dt.Tables[7].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[7].Rows[0][0]) : 0;
+            profiler.Month_count = (dt.Tables[8].Rows.Count != 0) ? Convert.ToInt32(dt.Tables[8].Rows[0][0]) : 0;
 
             return new GetProfilersResponse { Profiler = profiler };
         }
