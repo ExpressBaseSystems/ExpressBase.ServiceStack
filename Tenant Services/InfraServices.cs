2b5fec98f38f4b04e8de8e00a2731433e1c318e5
NithinVasudev <nithin.tp@expressbase.com>
2019-07-10 16:39:17 +0530

    Admin side added

--- Tenant Services/InfraServices.cs
+++ Tenant Services/InfraServices.cs
@@ -206,7 +206,7 @@ namespace ExpressBase.ServiceStack.Services
             {
                 string sid = "SELECT * from eb_sid_gen();";
                 EbDataTable dt1 = this.InfraConnectionFactory.DataDB.DoQuery(sid);
-                string Sol_id_autogen = Convert.ToString(dt1.Rows[0][0]);
+                string Sol_id_autogen = "admin";
 
                 string sql = @"INSERT INTO eb_solutions
                                             (
