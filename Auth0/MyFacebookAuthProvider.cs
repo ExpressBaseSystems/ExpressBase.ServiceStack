3efbe6bfa4c11727490826829f93afed442f882b
VarghesePaulose <51479206+VarghesePaulose@users.noreply.github.com>
2019-07-30 14:54:05 +0530

    facebook log

--- Auth0/MyFacebookAuthProvider.cs
+++ Auth0/MyFacebookAuthProvider.cs
@@ -175,105 +175,123 @@ namespace ExpressBase.ServiceStack.Auth0
 
         public override string CreateOrMergeAuthSession(IAuthSession session, IAuthTokens tokens)
         {
-            return base.CreateOrMergeAuthSession(session, tokens);
+			Console.WriteLine("reached CreateOrMergeAuthSession ");
+			return base.CreateOrMergeAuthSession(session, tokens);
         }
 
         public override bool Equals(object obj)
         {
-            return base.Equals(obj);
+			Console.WriteLine("reached Equals ");
+			return base.Equals(obj);
         }
 
         public override int GetHashCode()
         {
-            return base.GetHashCode();
+			Console.WriteLine("reached GetHashCode ");
+			return base.GetHashCode();
         }
 
         public override bool IsAccountLocked(IAuthRepository authRepo, IUserAuth userAuth, IAuthTokens tokens = null)
         {
-            return base.IsAccountLocked(authRepo, userAuth, tokens);
+			Console.WriteLine("reached IsAccountLocked ");
+			return base.IsAccountLocked(authRepo, userAuth, tokens);
         }
 
         public override bool IsAuthorized(IAuthSession session, IAuthTokens tokens, Authenticate request = null)
         {
-            return base.IsAuthorized(session, tokens, request);
+			Console.WriteLine("reached IsAuthorized ");
+			return base.IsAuthorized(session, tokens, request);
         }
 
         public override void LoadUserOAuthProvider(IAuthSession authSession, IAuthTokens tokens)
         {
-            base.LoadUserOAuthProvider(authSession, tokens);
+			Console.WriteLine("reached LoadUserOAuthProvider ");
+			base.LoadUserOAuthProvider(authSession, tokens);
         }
 
         public override object Logout(IServiceBase service, Authenticate request)
         {
-            return base.Logout(service, request);
+			Console.WriteLine("reached Logout ");
+			return base.Logout(service, request);
         }
 
         public override IHttpResult OnAuthenticated(IServiceBase authService, IAuthSession session, IAuthTokens tokens, Dictionary<string, string> authInfo)
         {
-            return base.OnAuthenticated(authService, session, tokens, authInfo);
+			Console.WriteLine("reached OnAuthenticated ");
+			return base.OnAuthenticated(authService, session, tokens, authInfo);
         }
 
         public override void OnFailedAuthentication(IAuthSession session, IRequest httpReq, IResponse httpRes)
         {
-            base.OnFailedAuthentication(session, httpReq, httpRes);
+			Console.WriteLine("reached OnFailedAuthentication ");
+			base.OnFailedAuthentication(session, httpReq, httpRes);
         }
 
         public override Task OnFailedAuthenticationAsync(IAuthSession session, IRequest httpReq, IResponse httpRes)
         {
-            return base.OnFailedAuthenticationAsync(session, httpReq, httpRes);
+			Console.WriteLine("reached OnFailedAuthenticationAsync ");
+			return base.OnFailedAuthenticationAsync(session, httpReq, httpRes);
         }
 
         public override string ToString()
         {
-            return base.ToString();
+			Console.WriteLine("reached ToString ");
+			return base.ToString();
         }
 
         protected override object AuthenticateWithAccessToken(IServiceBase authService, IAuthSession session, IAuthTokens tokens, string accessToken)
         {
-            return base.AuthenticateWithAccessToken(authService, session, tokens, accessToken);
+			Console.WriteLine("reached AuthenticateWithAccessToken ");
+			return base.AuthenticateWithAccessToken(authService, session, tokens, accessToken);
         }
 
         protected override object ConvertToClientError(object failedResult, bool isHtml)
         {
-            return base.ConvertToClientError(failedResult, isHtml);
+			Console.WriteLine("reached ConvertToClientError ");
+			return base.ConvertToClientError(failedResult, isHtml);
         }
 
         protected override bool EmailAlreadyExists(IAuthRepository authRepo, IUserAuth userAuth, IAuthTokens tokens = null)
         {
-            return base.EmailAlreadyExists(authRepo, userAuth, tokens);
+			Console.WriteLine("reached EmailAlreadyExists ");
+			return base.EmailAlreadyExists(authRepo, userAuth, tokens);
         }
 
         protected override string GetAuthRedirectUrl(IServiceBase authService, IAuthSession session)
         {
-            return base.GetAuthRedirectUrl(authService, session);
+			Console.WriteLine("reached GetAuthRedirectUrl ");
+			return base.GetAuthRedirectUrl(authService, session);
         }
 
         protected override IAuthRepository GetAuthRepository(IRequest req)
         {
-            return base.GetAuthRepository(req);
+			Console.WriteLine("reached GetAuthRepository ");
+			return base.GetAuthRepository(req);
         }
 
         protected override string GetReferrerUrl(IServiceBase authService, IAuthSession session, Authenticate request = null)
 
-
-
         {
-            return base.GetReferrerUrl(authService, session, request);
+			Console.WriteLine("reached GetReferrerUrl ");
+			return base.GetReferrerUrl(authService, session, request);
         }
 
         protected override void LoadUserAuthInfo(AuthUserSession userSession, IAuthTokens tokens, Dictionary<string, string> authInfo)
         {
-            base.LoadUserAuthInfo(userSession, tokens, authInfo);
+			Console.WriteLine("reached LoadUserAuthInfo ");
+			base.LoadUserAuthInfo(userSession, tokens, authInfo);
         }
 
         protected override bool UserNameAlreadyExists(IAuthRepository authRepo, IUserAuth userAuth, IAuthTokens tokens = null)
         {
-            return base.UserNameAlreadyExists(authRepo, userAuth, tokens);
+			Console.WriteLine("reached UserNameAlreadyExists ");
+			return base.UserNameAlreadyExists(authRepo, userAuth, tokens);
         }
 
         protected override IHttpResult ValidateAccount(IServiceBase authService, IAuthRepository authRepo, IAuthSession session, IAuthTokens tokens)
         {
-            return base.ValidateAccount(authService, authRepo, session, tokens);
+			Console.WriteLine("reached ValidateAccount ");
+			return base.ValidateAccount(authService, authRepo, session, tokens);
         }
     }
 }
