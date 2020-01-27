469f65cf2bca0a736314ba4af4d4e5812fe992a2
Dona Jose <donajose@expressbase.com>
2020-01-27 16:20:22 +0530

    IotService

---
+++ Services/IoTService.cs
@@ -0,0 +1,13 @@
+﻿using ExpressBase.Objects.ServiceStack_Artifacts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressBase.ServiceStack.Services
+{
+    public class IoTService : EbBaseService
+    {
+
+    }
+}
