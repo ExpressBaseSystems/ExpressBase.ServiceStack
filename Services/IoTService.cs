45493513a0f111a677fa7268e6e175a2cbb604ff
robyabraham <robyabraham@expressbase.com>
2020-01-27 19:02:39 +0530

    exception handler

--- Services/IoTService.cs
+++ Services/IoTService.cs
@@ -10,9 +10,16 @@ namespace ExpressBase.ServiceStack.Services
     {
         public object Any(IoTDataRequest request)
         {
-            request.SolnId = "ronds";
+            request.SolnId = "ebdblvnzp5spac20200127092930";
             string _sql = $"INSERT INTO ronds_sample(json) values({request.Data});";
-            this.EbConnectionFactory = new Common.Data.EbConnectionFactory(request.SolnId,this.Redis);
+            try
+            {
+                this.EbConnectionFactory = new Common.Data.EbConnectionFactory(request.SolnId, this.Redis);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("IoT -----------------"+ e.Message + "Stacktrace:----------"+e.StackTrace);
+            }
             int result = this.EbConnectionFactory.ObjectsDB.DoNonQuery(_sql);
             return new IoTDataResponse();
         }
