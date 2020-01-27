c5abfb2cc25a064cd571e9fc9b0b7ae8a37c3bb1
robyabraham <robyabraham@expressbase.com>
2020-01-27 17:49:14 +0530

    New IoT service

--- Services/IoTService.cs
+++ Services/IoTService.cs
@@ -8,6 +8,11 @@ namespace ExpressBase.ServiceStack.Services
 {
     public class IoTService : EbBaseService
     {
-
+        public object Any(IoTDataRequest request)
+        {
+            string _sql = $"INSERT INTO ronds_sample(json) values({request.Data});";
+            int result = this.EbConnectionFactory.ObjectsDB.DoNonQuery(_sql);
+            return new IoTDataResponse();
+        }
     }
 }
