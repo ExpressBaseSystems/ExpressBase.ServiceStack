58386ebe651e2cf9e753811a7273070cdb5d4bd1
anoopa <anoopa.baby@expressbase.com>
2020-02-27 13:22:02 +0530

    Minor

--- Services/SecurityServices.cs
+++ Services/SecurityServices.cs
@@ -373,19 +373,26 @@ namespace ExpressBase.ServiceStack.Services
             //	}
             //}
 
-            if(request.Id == 0 && id > 0)
+            if (request.Id == 0 && id > 0)
             {
-                var service = base.ResolveService<NotificationService>();
-                var r = (NotifyByUserRoleResponse)service.Post(new NotifyByUserRoleRequest
+                try
                 {
-                    Link = $"/Security/ManageUser?itemid={id}&Mode=2",
-                    Title = "New User "+ request.FullName + " Created" ,
-                    RoleID = new List<int>
+                    var service = base.ResolveService<NotificationService>();
+                    var r = (NotifyByUserRoleResponse)service.Post(new NotifyByUserRoleRequest
+                    {
+                        Link = $"/Security/ManageUser?itemid={id}&Mode=2",
+                        Title = "New User " + request.FullName + " Created",
+                        RoleID = new List<int>
                             {
                                 (int)SystemRoles.SolutionOwner,
                                 (int)SystemRoles.SolutionAdmin
                             }
-                });
+                    });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + e.StackTrace);
+                }
             }
 
             return resp;
