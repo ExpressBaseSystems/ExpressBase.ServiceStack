89c399c1f9cf45d65e72b60edc53f5e2e880e0a0
Dona Jose <donajose@expressbase.com>
2020-08-11 10:25:31 +0530

    Log

--- MQServices/PdfToEmailService.cs
+++ MQServices/PdfToEmailService.cs
@@ -111,6 +111,7 @@ namespace ExpressBase.ServiceStack.MQServices
                             Console.WriteLine("EmailTemplateWithAttachment.matches filled");
                             foreach (EbDataTable dt in ds.Tables)
                             {
+                                Console.WriteLine("EmailTemplateWithAttachment.ToColumnName:" + EmailTemplate.To);
                                 mailTo = dt.Rows[0][EmailTemplate.To.Split('.')[1]].ToString();
                             }
                             Console.WriteLine("EmailTemplateWithAttachment.mailTo = " + mailTo);
