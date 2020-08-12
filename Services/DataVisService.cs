d1ba7dc61cef3592092b81957e30676f911be083
robyabraham <robyabraham@expressbase.com>
2020-08-12 18:15:37 +0530

    calendar view minor fix

--- Services/DataVisService.cs
+++ Services/DataVisService.cs
@@ -1058,10 +1058,10 @@ namespace ExpressBase.ServiceStack
                 for (int i = 0; i < MasterRows.Count; i++)
                 {
                     object keydata = MasterRows[i][PrimaryColumn.OIndex];
-                    List<EbDataRow> customRows = LinesRows.FindAll(row => row[ForeignColumn.OIndex].Equals(keydata));
+                    List<EbDataRow> customRows = LinesRows.FindAll(row => Convert.ToInt32(row[ForeignColumn.OIndex]).Equals(keydata));
                     _formattedTable.Rows.Add(_formattedTable.NewDataRow2());
                     _formattedTable.Rows[i][_formattedTable.Columns.Count - 1] = i + 1;//serial
-
+                    
                     DataTable2FormatedTable4Calendar(MasterRows[i], customRows, _dv, _user_culture, _user, ref _formattedTable, ref globals, i, _hourCount, DateColumn);
                 }
                 return new PrePrcessorReturn { FormattedTable = _formattedTable, rows = MasterRows };
