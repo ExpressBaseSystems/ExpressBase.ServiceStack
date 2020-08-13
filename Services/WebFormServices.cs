ad5961efdcb002c139c40fd70dce23418c744a32
Febin Carlos <febincarlos@expressbase.com>
2020-08-13 18:06:54 +0530

    Type mismatch msg - exceptional case for Int32

--- Services/WebFormServices.cs
+++ Services/WebFormServices.cs
@@ -243,11 +243,12 @@ namespace ExpressBase.ServiceStack.Services
                                 (entry.Type.EbDbType.ToString().Equals("Boolean") && dr.Type.ToString().Equals("String")) ||
                                 (entry.Type.EbDbType.ToString().Equals("BooleanOriginal") && dr.Type.ToString().Equals("Boolean")) ||
                                 (entry.Type.EbDbType.ToString().Equals("Decimal") && (dr.Type.ToString().Equals("Int32") || dr.Type.ToString().Equals("Int64"))) ||
+                                (entry.Type.EbDbType.ToString().Equals("Int32") && dr.Type.ToString().Equals("Decimal")) ||
                                 (entry.Type.EbDbType.ToString().Equals("DateTime") && dr.Type.ToString().Equals("Date")) ||
                                 (entry.Type.EbDbType.ToString().Equals("Date") && dr.Type.ToString().Equals("DateTime")) ||
                                 (entry.Type.EbDbType.ToString().Equals("Time") && dr.Type.ToString().Equals("DateTime"))
                                 ))
-                                Msg += $"Type mismatch found '{dr.Type.ToString()}' instead of '{entry.Type.EbDbType}' for {tableName}.{entry.Name}; ";
+                                Msg += $"Type mismatch found '{dr.Type}' instead of '{entry.Type.EbDbType}' for {tableName}.{entry.Name}; ";
                             //Msg += string.Format("Already exists '{0}' Column for {1}.{2}({3}); ", dr.Type.ToString(), tableName, entry.Name, entry.Type.EbDbType);                           
                             isFound = true;
                             break;
