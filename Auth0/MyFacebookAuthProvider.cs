2d5195a9bb929b97af9065bd1178e3c179f7bc84
VarghesePaulose <51479206+VarghesePaulose@users.noreply.github.com>
2020-08-21 12:40:27 +0530

    changed staging url .xyz to cloud

--- Auth0/MyFacebookAuthProvider.cs
+++ Auth0/MyFacebookAuthProvider.cs
@@ -203,7 +203,7 @@
 //							};
 //							b = JsonConvert.SerializeObject(sco_signup);
 //							string sociallink1 = "localhost:";
-//							string sociallink2 = "eb-test.xyz";
+//							string sociallink2 = "eb-test.cloud";
 //							string sociallink3 = "expressbase.com";
 //							Console.WriteLine("ReferrerUrl= " + session.ReferrerUrl);
 //							if (urllink.Contains(sociallink1, StringComparison.OrdinalIgnoreCase))
@@ -215,8 +215,8 @@
 
 //							if (urllink.Contains(sociallink2, StringComparison.OrdinalIgnoreCase))
 //							{
-//								Console.WriteLine("reached  redirect to myaccount.eb-test.xyz");
-//								return authService.Redirect(SuccessRedirectUrlFilter(this, string.Format("https://myaccount.eb-test.xyz/social_oauth?scosignup={0}", b)));
+//								Console.WriteLine("reached  redirect to myaccount.eb-test.cloud");
+//								return authService.Redirect(SuccessRedirectUrlFilter(this, string.Format("https://myaccount.eb-test.cloud/social_oauth?scosignup={0}", b)));
 //							}
 
 //							if (urllink.Contains(sociallink3, StringComparison.OrdinalIgnoreCase))
