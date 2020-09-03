b5baeb505dda8d7786901ad8de0d37cca5af8551
Suresh Balasubramanian <sureba@expressbase.com>
2020-09-04 02:36:57 +0530

    debugging again

--- Services/SecurityServices.cs
+++ Services/SecurityServices.cs
@@ -972,6 +972,7 @@ namespace ExpressBase.ServiceStack.Services
         //----MANAGE ROLES START---------------------------------------
         public GetManageRolesResponse Any(GetManageRolesRequest request)
         {
+	GetManageRolesResponse resp = null;
 	try{
             string query = null;
 
@@ -1057,8 +1058,9 @@ namespace ExpressBase.ServiceStack.Services
                     temp_locs += dr[0].ToString() + ",";
                 RoleInfo.Add("LocationIds", string.IsNullOrEmpty(temp_locs) ? "" : temp_locs.Substring(0, temp_locs.Length - 1));
             }
+	    resp = new GetManageRolesResponse() { ApplicationCollection = _applicationCollection, SelectedRoleInfo = RoleInfo, PermissionList = Permission, RoleList = _roleList, Role2RoleList = _r2rList, UsersList = _usersList, LocationList = _location };
 	    }catch(Exception e) { Console.WriteLine("ManageRolesRequest___999 " + e.ToString()); }
-            return new GetManageRolesResponse() { ApplicationCollection = _applicationCollection, SelectedRoleInfo = RoleInfo, PermissionList = Permission, RoleList = _roleList, Role2RoleList = _r2rList, UsersList = _usersList, LocationList = _location };
+            return resp;
         }
 
         public GetUserDetailsResponse Any(GetUserDetailsRequest request)
