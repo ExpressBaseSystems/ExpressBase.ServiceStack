a46ce1705351aef06ed35107eb3dc84c4deca4ed
Dona Jose <donajose@expressbase.com>
2021-10-26 19:55:46 +0530

    Check for phone number length in sms

--- MQServices/SMSService.cs
+++ MQServices/SMSService.cs
@@ -141,7 +141,7 @@ namespace ExpressBase.ServiceStack.MQServices
         {
             SmsCreateService SmsCreateService = base.ResolveService<SmsCreateService>();
             FilledSmsTemplate FilledSmsTemplate = SmsCreateService.FillSmsTemplate(request);
-            if (FilledSmsTemplate != null && FilledSmsTemplate.SmsTemplate != null && !string.IsNullOrEmpty(FilledSmsTemplate.SmsTo))
+            if (!(FilledSmsTemplate?.SmsTemplate is null) && !string.IsNullOrEmpty(FilledSmsTemplate?.SmsTo) && FilledSmsTemplate?.SmsTo.Length > 1)
                 try
                 {
                     this.MessageProducer3.Publish(new SMSSentRequest
@@ -168,11 +168,11 @@ namespace ExpressBase.ServiceStack.MQServices
         {
             EbConnectionFactory ebConnectionFactory = new EbConnectionFactory(request.SolnId, this.Redis);
             RetrySmsResponse response = new RetrySmsResponse();
-           
+
             string sql = string.Format(@"SELECT send_to, send_from, message_body, status, result, 
 		                        metadata, retryof, con_id, eb_created_by, eb_created_at
 	                        FROM eb_sms_logs 
-                            WHERE eb_del = 'F' AND id = @id AND refid = @refid" );
+                            WHERE eb_del = 'F' AND id = @id AND refid = @refid");
             DbParameter[] parameteres = {ebConnectionFactory.DataDB.GetNewParameter("id", EbDbTypes.Int32, request.SmslogId),
                                     ebConnectionFactory.DataDB.GetNewParameter("refid", EbDbTypes.String, request.RefId)};
             EbDataTable dt = ebConnectionFactory.ObjectsDB.DoQuery(sql, parameteres);
@@ -195,78 +195,78 @@ namespace ExpressBase.ServiceStack.MQServices
             return response;
         }
 
-            //[Route("/callback/{apikey}")]
-            //public class String : IReturn<IReturnVoid>
-            //{
-            //    public String apikey { get; set; }
-            //}
+        //[Route("/callback/{apikey}")]
+        //public class String : IReturn<IReturnVoid>
+        //{
+        //    public String apikey { get; set; }
+        //}
 
 
-            //public void Any(SMSService.String apikey)
-            //{
+        //public void Any(SMSService.String apikey)
+        //{
 
-            // }
+        // }
 
-            //[Restrict(InternalOnly = true)]
-            //public class SMSServiceInternal : EbBaseService
-            //{
-            //    public SMSServiceInternal(IMessageProducer _mqp) : base(_mqp) { }
+        //[Restrict(InternalOnly = true)]
+        //public class SMSServiceInternal : EbBaseService
+        //{
+        //    public SMSServiceInternal(IMessageProducer _mqp) : base(_mqp) { }
 
-            //    public string Post(SMSSentMqRequest req)
-            //    {
-            //        EbConnectionFactory dbFactory = new EbConnectionFactory(req.TenantAccountId, this.Redis);
+        //    public string Post(SMSSentMqRequest req)
+        //    {
+        //        EbConnectionFactory dbFactory = new EbConnectionFactory(req.TenantAccountId, this.Redis);
 
-            //        var MsgStatus = dbFactory.SMSConnection.SendSMS(req.To, req.From, req.Body);
+        //        var MsgStatus = dbFactory.SMSConnection.SendSMS(req.To, req.From, req.Body);
 
-            //        SMSStatusLogMqRequest logMqRequest = new SMSStatusLogMqRequest();
-            //        logMqRequest.SMSSentStatus = new SMSSentStatus();
+        //        SMSStatusLogMqRequest logMqRequest = new SMSStatusLogMqRequest();
+        //        logMqRequest.SMSSentStatus = new SMSSentStatus();
 
-            //        foreach (var Info in MsgStatus)
-            //        {
-            //            if (Info.Key == "To")
-            //                logMqRequest.SMSSentStatus.To = Info.Value;
-            //            if (Info.Key == "From")
-            //                logMqRequest.SMSSentStatus.From = Info.Value;
-            //            if (Info.Key == "Uri")
-            //                logMqRequest.SMSSentStatus.Uri = Info.Value;
-            //            if (Info.Key == "Body")
-            //                logMqRequest.SMSSentStatus.Body = Info.Value;
-            //            if (Info.Key == "Status")
-            //                logMqRequest.SMSSentStatus.Status = Info.Value;
-            //            if (Info.Key == "SentTime")
-            //                //logMqRequest.SMSSentStatus.SentTime = DateTime.Parse(Info.Value);
-            //                if (Info.Key == "ErrorMessage")
-            //                    logMqRequest.SMSSentStatus.ErrorMessage = Info.Value;
-            //        }
-            //        logMqRequest.UserId = req.UserId;
-            //        logMqRequest.TenantAccountId = req.TenantAccountId;
+        //        foreach (var Info in MsgStatus)
+        //        {
+        //            if (Info.Key == "To")
+        //                logMqRequest.SMSSentStatus.To = Info.Value;
+        //            if (Info.Key == "From")
+        //                logMqRequest.SMSSentStatus.From = Info.Value;
+        //            if (Info.Key == "Uri")
+        //                logMqRequest.SMSSentStatus.Uri = Info.Value;
+        //            if (Info.Key == "Body")
+        //                logMqRequest.SMSSentStatus.Body = Info.Value;
+        //            if (Info.Key == "Status")
+        //                logMqRequest.SMSSentStatus.Status = Info.Value;
+        //            if (Info.Key == "SentTime")
+        //                //logMqRequest.SMSSentStatus.SentTime = DateTime.Parse(Info.Value);
+        //                if (Info.Key == "ErrorMessage")
+        //                    logMqRequest.SMSSentStatus.ErrorMessage = Info.Value;
+        //        }
+        //        logMqRequest.UserId = req.UserId;
+        //        logMqRequest.TenantAccountId = req.TenantAccountId;
 
-            //        this.MessageProducer3.Publish(logMqRequest);
-            //        return null;
-            //    }
+        //        this.MessageProducer3.Publish(logMqRequest);
+        //        return null;
+        //    }
 
-            //    public string Post(SMSStatusLogMqRequest request)
-            //    {
-            //        EbConnectionFactory connectionFactory = new EbConnectionFactory(request.TenantAccountId, this.Redis);
+        //    public string Post(SMSStatusLogMqRequest request)
+        //    {
+        //        EbConnectionFactory connectionFactory = new EbConnectionFactory(request.TenantAccountId, this.Redis);
 
-            //        string sql = "INSERT INTO logs_sms(uri, send_to, send_from, message_body, status, error_message, user_id, context_id) VALUES (@uri, @to, @from, @message_body, @status, @error_message, @user_id, @context_id) RETURNING id";
+        //        string sql = "INSERT INTO logs_sms(uri, send_to, send_from, message_body, status, error_message, user_id, context_id) VALUES (@uri, @to, @from, @message_body, @status, @error_message, @user_id, @context_id) RETURNING id";
 
-            //        DbParameter[] parameters =
-            //                {
-            //                connectionFactory.DataDB.GetNewParameter("uri", EbDbTypes.String, string.IsNullOrEmpty(request.SMSSentStatus.Uri)?string.Empty:request.SMSSentStatus.Uri),
-            //                connectionFactory.DataDB.GetNewParameter("to",EbDbTypes.String, request.SMSSentStatus.To),
-            //                connectionFactory.DataDB.GetNewParameter("from",EbDbTypes.String, request.SMSSentStatus.From),
-            //                connectionFactory.DataDB.GetNewParameter("message_body",EbDbTypes.String, string.IsNullOrEmpty(request.SMSSentStatus.Body)?string.Empty:request.SMSSentStatus.Body),
-            //                connectionFactory.DataDB.GetNewParameter("status",EbDbTypes.String, string.IsNullOrEmpty(request.SMSSentStatus.Status)?string.Empty:request.SMSSentStatus.Status),
-            //                //connectionFactory.DataDB.GetNewParameter("sent_time",System.Data.DbType.DateTime, request.SMSSentStatus.SentTime),
-            //                connectionFactory.DataDB.GetNewParameter("error_message",EbDbTypes.String, string.IsNullOrEmpty(request.SMSSentStatus.ErrorMessage)?string.Empty:request.SMSSentStatus.ErrorMessage),
-            //                connectionFactory.DataDB.GetNewParameter("user_id",EbDbTypes.Int32, request.UserId),
-            //                connectionFactory.DataDB.GetNewParameter("context_id",EbDbTypes.Int32, string.IsNullOrEmpty(request.ContextId.ToString())?request.UserId:request.ContextId)
-            //                };
-            //        var iCount = connectionFactory.DataDB.DoQuery(sql, parameters);
+        //        DbParameter[] parameters =
+        //                {
+        //                connectionFactory.DataDB.GetNewParameter("uri", EbDbTypes.String, string.IsNullOrEmpty(request.SMSSentStatus.Uri)?string.Empty:request.SMSSentStatus.Uri),
+        //                connectionFactory.DataDB.GetNewParameter("to",EbDbTypes.String, request.SMSSentStatus.To),
+        //                connectionFactory.DataDB.GetNewParameter("from",EbDbTypes.String, request.SMSSentStatus.From),
+        //                connectionFactory.DataDB.GetNewParameter("message_body",EbDbTypes.String, string.IsNullOrEmpty(request.SMSSentStatus.Body)?string.Empty:request.SMSSentStatus.Body),
+        //                connectionFactory.DataDB.GetNewParameter("status",EbDbTypes.String, string.IsNullOrEmpty(request.SMSSentStatus.Status)?string.Empty:request.SMSSentStatus.Status),
+        //                //connectionFactory.DataDB.GetNewParameter("sent_time",System.Data.DbType.DateTime, request.SMSSentStatus.SentTime),
+        //                connectionFactory.DataDB.GetNewParameter("error_message",EbDbTypes.String, string.IsNullOrEmpty(request.SMSSentStatus.ErrorMessage)?string.Empty:request.SMSSentStatus.ErrorMessage),
+        //                connectionFactory.DataDB.GetNewParameter("user_id",EbDbTypes.Int32, request.UserId),
+        //                connectionFactory.DataDB.GetNewParameter("context_id",EbDbTypes.Int32, string.IsNullOrEmpty(request.ContextId.ToString())?request.UserId:request.ContextId)
+        //                };
+        //        var iCount = connectionFactory.DataDB.DoQuery(sql, parameters);
 
-            //        return null;
-            //    }
-            //}
-        }
+        //        return null;
+        //    }
+        //}
+    }
 }
