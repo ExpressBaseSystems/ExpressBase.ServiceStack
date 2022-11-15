5ae0966e8fd30d8bf8008a2ef2842f27377734ba
donajose <donajose@expressbase.com>
2022-11-15 21:34:46 +0530

    Api in ps related

--- Services/ApiConversionService.cs
+++ Services/ApiConversionService.cs
@@ -5,6 +5,8 @@ using ExpressBase.Objects;
 using ExpressBase.Objects.ServiceStack_Artifacts;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Serialization.Json;
 using ServiceStack;
 using System;
 using System.Collections.Generic;
@@ -15,6 +17,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
+using ResponseStatus = ServiceStack.ResponseStatus;
 
 namespace ExpressBase.ServiceStack.Services
 {
@@ -31,38 +34,37 @@ namespace ExpressBase.ServiceStack.Services
             return resp;
         }
 
-        public HttpResponseMessage Execute(ApiConversionRequest request)
+        public IRestResponse Execute(ApiConversionRequest request)
         {
             List<Param> param = (request.Parameters != null) ? request.Parameters : new List<Param>();
-            string Url = request.Url;
-            if (request.Method == ApiMethods.GET && param.Count > 0)
-                Url = ModifyUrl(Url, param);
-            var uri = new Uri(Url);
-            HttpResponseMessage response = null;
+            string Url = (request.Method == ApiMethods.GET && param.Count > 0) ? ModifyUrl(request.Url, param) : request.Url;
+            Uri uri = new Uri(Url);
+            IRestResponse response = null;
             try
             {
-                using (var client = new HttpClient())
-                {
-                    if (request.Headers != null && request.Headers.Any())
-                    {
-                        foreach (ApiRequestHeader header in request.Headers)
-                        {
-                            client.DefaultRequestHeaders.Add(header.HeaderName, header.Value);
-                        }
-                    }
+                RestClient client = new RestClient(uri.GetLeftPart(UriPartial.Authority));
+                Method Method = (request.Method == ApiMethods.POST) ? RestSharp.Method.POST : RestSharp.Method.GET;
+                RestRequest rest_req = new RestRequest(uri.PathAndQuery, Method);
 
-                    client.BaseAddress = new Uri(uri.GetLeftPart(System.UriPartial.Authority));
-                    if (request.Method == ApiMethods.POST)
+                if (request.Headers != null && request.Headers.Any())
+                {
+                    foreach (ApiRequestHeader header in request.Headers)
                     {
-                        var parameters = param.Select(i => new { prop = i.Name, val = i.Value })
-                                .ToDictionary(x => x.prop, x => x.val);
-                        response = client.PostAsync(uri.PathAndQuery, new FormUrlEncodedContent(parameters)).Result;
-                    }
-                    else if (request.Method == ApiMethods.GET)
-                    {
-                        response = client.GetAsync(uri.PathAndQuery).Result;
+                        rest_req.AddHeader(header.HeaderName, header.Value);
                     }
                 }
+
+                if (uri.GetLeftPart(System.UriPartial.Authority).Contains(RoutingConstants.LIVEHOSTADDRESS) ||
+                    uri.GetLeftPart(System.UriPartial.Authority).Contains(RoutingConstants.STAGEHOSTADDRESS))
+                {
+                    rest_req.AddHeader("bToken", this.Request.Headers["Authorization"]);
+                    rest_req.AddHeader("rToken", this.Request.Headers["rToken"]);
+                }
+                foreach (Param _param in param)
+                {
+                    rest_req.AddParameter(_param.Name, _param.ValueTo);
+                }
+                response = client.Execute(rest_req);
             }
             catch (Exception e)
             {
@@ -92,12 +94,25 @@ namespace ExpressBase.ServiceStack.Services
         private ApiConversionResponse start_get(ApiConversionRequest request)
         {
             ApiConversionResponse resp = new ApiConversionResponse();
-            HttpResponseMessage response = null;
+            IRestResponse response = null;
             try
             {
                 response = Execute(request);
-                JObject jsonObject = JObject.Parse(response.Content.ReadAsStringAsync().Result);
-                List<JProperty> Jproperty = jsonObject.Properties().Where(pp => pp.Value.Type == JTokenType.Array).ToList();
+                string rawResponse = response.Content;
+                JObject jsonObject = JObject.Parse(rawResponse);
+                List<JProperty> Jproperty;
+                Uri uri = new Uri(request.Url);
+                if (uri.GetLeftPart(System.UriPartial.Authority).Contains(RoutingConstants.LIVEHOSTADDRESS) ||
+                    uri.GetLeftPart(System.UriPartial.Authority).Contains(RoutingConstants.STAGEHOSTADDRESS))
+                {
+                    List<JProperty> Jproperty1 = jsonObject.Properties().Where(pp => pp.Name == "result").ToList();
+                    JObject data = JObject.Parse(Jproperty1[0].Value.ToString());
+                    Jproperty = data.Properties().Where(pp => pp.Name == "data").ToList();
+
+                }
+                else
+                    Jproperty = jsonObject.Properties().Where(pp => pp.Value.Type == JTokenType.Array).ToList();
+
                 foreach (JProperty property in Jproperty)
                     GetRecursive(property);
             }
@@ -143,8 +158,9 @@ namespace ExpressBase.ServiceStack.Services
 
         private void GetRecursive(JProperty property)
         {
-            int k = 0;int i = 0;
+            int k = 0; int i = 0;
             ds.Tables.Add(new EbDataTable(property.Name));
+            JTokenType _type;
             foreach (var children in property.Value.Children())
             {
                 if (i == 0)
@@ -156,17 +172,34 @@ namespace ExpressBase.ServiceStack.Services
                             GetRecursive(prop);
                         else
                         {
-                            ds.Tables[property.Name].Columns.Add(new EbDataColumn { ColumnIndex = k, ColumnName = prop.Name.Replace(" ","_"), Type = ConvertToEbdbType(prop.Value.Type) });
-                            ds.Tables[property.Name].Rows[i][k++] = ConvertValueToEbdbType(prop.Value.Type, prop.Value);
+                            if (prop.Name == "id" && Int32.TryParse((string)prop.Value, out _))
+                            {
+                                _type = JTokenType.Integer;
+                            }
+                            else
+                            {
+                                _type = prop.Value.Type;
+                            }
+                            ds.Tables[property.Name].Columns.Add(new EbDataColumn { ColumnIndex = k, ColumnName = prop.Name.Replace(" ", "_"), Type = ConvertToEbdbType(_type) });
+
+                            ds.Tables[property.Name].Rows[i][k++] = ConvertValueToEbdbType(_type, prop.Value);
                         }
                     }
                 }
                 else
                 {
                     ds.Tables[property.Name].Rows.Add(ds.Tables[property.Name].NewDataRow2());
-                    k = 0;
+                    k = 0; 
                     foreach (var prop in children.Children<JProperty>().ToArray())
-                    {
+                    {                        
+                        if (prop.Name == "id" && Int32.TryParse((string)prop.Value, out _))
+                        {
+                            _type = JTokenType.Integer;
+                        }
+                        else
+                        {
+                            _type = prop.Value.Type;
+                        }
                         ds.Tables[property.Name].Rows[i][k++] = ConvertValueToEbdbType(prop.Value.Type, prop.Value);
                     }
                 }
